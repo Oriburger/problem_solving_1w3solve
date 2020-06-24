afc4bcb57afdc9645667750ea039fb816410fe1f
4_Lider <uss425@naver.com>
2020-06-24 22:47:08 +0900

    Create 10972_다음순열.cpp

---
+++ Sources/10972_다음순열.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<int> arr;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin>>n;
+
+	for(int i=0; i<n; i++)
+	{
+		int input;
+		cin>>input;
+		arr.push_back(input);
+	}
+
+	bool flag = next_permutation(arr.begin(), arr.end());
+
+	if(flag)
+		for(int i=0; i<n; i++)
+			cout<<arr[i]<<' ';
+
+	else
+		cout<<-1<<'\n';
+
+	return 0;
+}
