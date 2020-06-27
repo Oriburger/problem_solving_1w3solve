d0bc01ced485713fa32d1f4a0c5fdfa7b75e4997
4_Lider <uss425@naver.com>
2020-06-27 20:17:16 +0900

    Update 1700_멀티탭스케줄링.cpp

--- Sources/Undone/1700_멀티탭스케줄링.cpp
+++ Sources/Undone/1700_멀티탭스케줄링.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 using namespace std;
 
-int n, k, mtap, ans=0; //n, k, 멀티탭 남은 칸 수, 정답
+int n, k, remaining, ans=0; //n, k, 멀티탭 남은 칸 수, 정답
 vector<int> schedule; //장치 사용 순서
 vector<bool> check; //현재 멀티탭에 꼽혀 있는지? 
-vector<int> cnt;
+vector<int> cnt; //장치별 스케쥴링 될 총 횟수
 
 int main()
 {
@@ -14,7 +14,7 @@ int main()
 	
 	cin>>n>>k;
 	
-	mtap=n;
+	remaining=n;
 	check.resize(k+1);
 	cnt.resize(k+1);
 	for(int i=0; i<k; i++)
@@ -27,66 +27,47 @@ int main()
 	
 	for(int i=0; i<k; i++)
 	{
-		if(check[schedule[i]]) //schedule[i]가 이미 꽂혀 있는 경우 
+		//이미 사용중인게 아니고, 공간이 남지 않았을 경우
+		if(!check[schedule[i]] && remaining==0)
 		{
-			cnt[schedule[i]]--;
-			continue;
-		}
-		
-		else if(mtap>0)  //구멍이 남아있는 경우 
-		{
-			check[schedule[i]]=true;
-			mtap--;
-			cnt[schedule[i]]--;
-		}
-		else //구멍이 남아있지 않은 경우 
-		{	
-			int minUse = 999; 
-			int minUseIdx = 0;
+			bool flag = false;
+			int lastIdx = 0;
 			
+			//현재 꽂혀 있고, 추후 사용하지 않을 장치인 경우
 			for(int j=1; j<=k; j++) 
 			{
-				if(check[j] && cnt[j]<minUse)
+				if(check[j] && cnt[j]==0)
 				{
-					minUseIdx = j;
-					minUse = cnt[j];
+					check[j]=false; //해당 장치를 뽑는다.
+					cout<<j<<" pop!!!\n";
+					flag = true; ans++;
+					break;
 				}
 			}
-			
-			if(minUse==0) //앞으로 사용하지 않을 장치라면, 
+
+			if(!flag) //현재 꽂혀있는 장치들이 모두 사용될 예정인 경우
 			{
-				ans++;
-				check[minUseIdx] = false;
-				check[schedule[i]] = true;	
-				cnt[schedule[i]]--;
-				
-				//cout<<minUseIdx<<" pop!\n";
-			}
-			
-			//꼽혀있는 장치들 중에, 그런 장치가 없다면
-			//가장 마지막에 쓸 장치를 뽑는다. 
-			else  
-			{
-				for(int j=k-1; j>i; j--)
+				for(int j=0; j<k; j++) 
 				{
 					if(check[schedule[j]])
-					{
-						ans++;
-						check[schedule[j]] = false;
-						check[schedule[i]] = true;	
-						cnt[schedule[i]]--;
-						
-						//cout<<schedule[j]<<" pop!\n";
-						
-						break;
-					}
+						lastIdx = schedule[j];
 				}
+				ans++;
+				
+				cout<<lastIdx<<" pop!\n";
+				check[lastIdx]=false;
 			}
 		}
-		//cout<<schedule[i]<<" push!\n";
+		remaining = remaining > 0 && schedule[i]!=check[i] ? remaining - 1 : 0; //빈칸에 꽂았을 경우에 값 갱신
+
+		cout<<schedule[i]<<" push!\n";
+		check[schedule[i]]=true; 
+		cnt[schedule[i]]--;
 	}
 	
 	cout<<ans<<'\n';
 	
 	return 0;
 }
+
+//https://dundung.tistory.com/90
