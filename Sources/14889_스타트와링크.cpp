9baf10e1dab86556e8b0a298e4ddb707123d546d
4_Lider <uss425@naver.com>
2020-06-29 19:00:23 +0900

    Update 14889_스타트와링크.cpp

--- Sources/14889_스타트와링크.cpp
+++ Sources/14889_스타트와링크.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, s[21][21];
+const int INF = 2147000000;
+
+int n, s[21][21], answer=INF;
+vector<int> c;
+
+int GetTeamStat(const int flag)
+{
+	int ret = 0;
+	vector<int> team;
+	
+	for(int i=0; i<n; i++)
+		if(c[i]==flag) team.push_back(i);
+
+	for(int i=0; i<team.size(); i++)
+		for(int j=i+1; j<team.size(); j++)
+			ret += s[team[i]][team[j]] + s[team[j]][team[i]];
+	
+	return ret;
+}
 
 int main()
 {
@@ -10,11 +30,25 @@ int main()
 	
 	cin>>n;
 	
-	for(int i=1; i<=n; i++)
-		for(int j=1; j<=n; j++)
+	for(int i=0; i<n; i++)
+	{
+		c.push_back(i>=n/2 ? 1 : 0);
+		for(int j=0; j<n; j++)
 			cin>>s[i][j];
+	}
 	
+	do
+	{
+		int statA=0, statB=0;
+		
+		statA = GetTeamStat(0);
+		statB = GetTeamStat(1);
+		
+		answer = min(answer, abs(statA-statB));
+		
+	}while(next_permutation(c.begin(), c.end()));
 	
+	cout<<answer<<'\n'; 
 	
 	return 0;
 }
