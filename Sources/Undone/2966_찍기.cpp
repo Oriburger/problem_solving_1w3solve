f712baa8e7e24dbbd7577124677ecbc8172baa81
4_Lider <uss425@naver.com>
2020-07-01 16:40:32 +0900

    Create 2966_찍기.cpp

---
+++ Sources/Undone/2966_찍기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string ans;
+
+string a = "ABC";
+string b = "BABC";
+string g = "CCAABB";
+
+int GetCorrect(string comp)
+{
+	for(int i=0; i<ans.size(); i++)
+	{
+		
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin>>n;
+	cin>>ans;
+
+	
+
+	return 0;
+}
