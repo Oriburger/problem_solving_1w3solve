65eb8cce85e3357adba0bba1898cf9f059b4c031
4_Lider <uss425@naver.com>
2020-07-02 19:50:50 +0900

    Create 4195_친구네트워크.cpp

---
+++ Sources/4195_친구네트워크.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+unordered_map<string, int> myMap;
+
+struct DisjointSet
+{
+	vector<int> parent, size, rank;
+
+	DisjointSet(int n):parent(n+1),size(n+1, 1),rank(n+1, 1)
+	{
+		for(int i=0; i<=n; i++)
+			parent[i]=i;
+	}
+
+	int Find(int u)
+	{
+		if(parent[u]==u) return parent[u];
+
+		return parent[u]=Find(parent[u]);
+	}
+
+	void Merge(int u, int v)
+	{
+		u = Find(u); v = Find(v);
+		if(u==v) return;
+
+		if(rank[u]>rank[v]) swap(u, v);
+		size[v] += size[u];
+		parent[u]=v; //랭크가 낮은 u를 v의 자식으로 넣음 
+		if(rank[u]==rank[v]) rank[v]+=1;
+	}
+
+	int GetSize(int u)
+	{
+		u = Find(u);
+
+		return size[u];
+	}
+};
+
+int t, f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin>>t;
+
+	while(t--)
+	{
+		cin>>f;
+
+		int cnt=0;
+		DisjointSet d(f);
+		for(int i=0; i<f; i++)
+		{
+			string str1, str2;
+			cin>>str1>>str2;
+
+			if(myMap.count(str1)==0)
+				myMap.insert({str1, cnt++});
+			if(myMap.count(str2)==0)
+				myMap.insert({str2, cnt++});
+
+			d.Merge(myMap[str1], myMap[str2]);
+			
+			cout<<d.GetSize(myMap[str1])<<'\n';
+		}
+
+		myMap.clear();
+	}
+
+	return 0;
+}
