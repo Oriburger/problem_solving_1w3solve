6bff6a34a079f41c7898ed5d07149f205fbe4007
Oriburger <uss425@naver.com>
2020-07-11 17:55:41 +0900

    Create 11651_좌표정렬하기2.cpp

---
+++ Sources/11651_좌표정렬하기2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Pos{ int y; int x; };
+
+bool CompPos(Pos a, Pos b)
+{
+	if(a.y < b.y) return true;
+	else if(a.y > b.y) return false;
+
+	else 
+	{
+		if(a.x < b.x) return true;
+		else if(a.x > b.x) return false;
+	}
+
+	return false;
+}
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin>>n;
+	vector<Pos> arr(n);
+
+	for(int i=0; i<n; i++)
+		cin>>arr[i].x>>arr[i].y;
+
+	sort(arr.begin(), arr.end(), CompPos);
+
+	for(int i=0; i<n; i++)
+		cout<<arr[i].x<<' '<<arr[i].y<<'\n';
+
+	return 0;
+}
