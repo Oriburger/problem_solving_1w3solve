4cb8fa0b615f3702f397604416e43a942958af72
Oriburger <uss425@naver.com>
2020-07-16 17:54:37 +0900

    Update 1408_24.cpp

--- Sources/Undone/1408_24.cpp
+++ Sources/Undone/1408_24.cpp
@@ -1,50 +1,28 @@
-#include <iostream>
+#include <cstdio>
 #include <string>
 using namespace std;
 
-string stStr, finStr;
-int st[3], fin[3], ans[3]; // hr min sec
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	int st[3], fin[3], ans[3]={0};
+	int stSec=0, finSec=0, ansSec=0;
+	char blank;
 
-	cin>>stStr>>finStr;
+	scanf("%d%c%d%c%d", &st[0], &blank, &st[1], &blank, &st[2]);
+	scanf("%d%c%d%c%d", &fin[0], &blank, &fin[1], &blank, &fin[2]);
 
-	for(int i=0, j=0; i<stStr.size(); i++)
-	{
-		if(i!=2 && i!=5)
-		{
-			st[j] = (stStr[i]-'0')*10;
-			fin[j] = (finStr[i]-'0')*10;
+	stSec = st[0]*3600+st[1]*60+st[2];
+	finSec = fin[0]*3600+fin[1]*60+fin[2];
 
-			i++;
+	if(finSec < stSec) finSec += 24*3600;
 
-			st[j] += stStr[i]-'0';
-			fin[j] += finStr[i]-'0';
-		}
-		else j++;
-	}
+	ansSec = finSec-stSec;
 
-	for(int i=0; i<3; i++)
-		cout<<ans[i]<<' ';
-		
-	for(int i=3; i>3; i++)
-	{
-		int temp = 0;
-
-		if(st[i]>fin[i])
-		{
-			ans[i] = fin[i]+60 - st[i];
-			fin[i-1] -= 1;
-		}
-		else
-			ans[i] = fin[i] - st[i];
-	}
-
-	for(int i=0; i<3; i++)
-		cout<<ans[i]<<' ';
+	ans[0] = ansSec / 3600;
+	ans[1] = (ansSec - ans[0]*3600) / 60;
+	ans[2] = ansSec - ans[0]*3600 - ans[1]*60;
+	printf("%02d:%02d:%02d", ans[0], ans[1], ans[2]);
 
 	return 0;
 }
