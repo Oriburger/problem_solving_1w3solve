78a414f55f437517920b0707a34173793cee59c8
Oriburger <uss425@naver.com>
2020-07-17 20:26:48 +0900

    Update 12757_전설의JBNU.cpp

--- Sources/Undone/12757_전설의JBNU.cpp
+++ Sources/Undone/12757_전설의JBNU.cpp
@@ -1,119 +1,118 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
 using namespace std;
 
 typedef vector<int>::iterator viter;
 
 int n, m, k;
-map<int, int> mData;
-vector<int> karr;
+map<int, int> JBNU, nJBNU;
+
+vector<int> FindKey(int key)
+{
+	map<int, int>::iterator iter = JBNU.lower_bound(key);
+	map<int, int>::iterator niter = nJBNU.lower_bound(-key);
+
+	//printf("-> %d %d\n", iter->first, niter->first);
+
+	if(abs(key - iter->first) > k && abs(key + (niter->first)) > k) return { };
+
+	else if(abs(key - iter->first) > k || abs(key + (niter->first)) > k  )
+	{
+		if(abs(key + (niter->first)) <= k)  
+			return { -((niter->first)) };
+		else if(abs(key + (iter->first)) <= k)  
+			return { iter->first };
+	}
+
+	else if(abs(key - iter->first) <= k 
+			&& abs(key + (niter->first)) <= k)
+	{
+		if(abs(key - iter->first) > abs(key + (niter->first))) 
+			return { -((niter->first))};
+		else if(abs(key - iter->first) < abs(key + (niter->first)))
+			return { iter->first };
+		else return { iter->first, -((niter->first)) };
+	}
+
+	return { };
+}
+
+void AddData(int key, int val)
+{
+	//중복 key 입력은 주어지지않음
+	JBNU.insert({key, val});
+	nJBNU.insert({-key, val});
+}
+
+void UpdateData(int key, int val)
+{
+	//조건을 만족하는 유일한 key가 있다면,
+	if(JBNU.count(key)) JBNU[key]=val;
+	
+	//그렇지 않다면
+	else
+	{
+		vector<int> keyArr = FindKey(key);
+
+		if(keyArr.size() != 1) return;
+
+		else JBNU[keyArr[0]]=val;
+	}
+}
+
+void PrtData(int key)
+{
+	//유일한 키가 있다면,
+	if(JBNU.count(key)==1)
+		cout<<JBNU[key]<<'\n';
+
+	else 
+	{
+		vector<int> keyArr = FindKey(key);
+
+		if(keyArr.size()==0) cout<<-1<<'\n';
+		else if(keyArr.size()==2) cout<<"?\n";
+		else if(keyArr.size()==1) cout<<JBNU[keyArr[0]]<<'\n';
+	}
+}
 
 int main()
 {
-	scanf("%d %d %d", &n, &m, &k);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin>>n>>m>>k;
 	
+	//초기 데이터 입력
 	for(int i=0; i<n; i++)
 	{
 		int key, value;
-		scanf("%d %d", &key, &value);
+		cin>>key>>value;
 		
-		karr.push_back(key);
-		mData.insert(make_pair(key, value));
+		AddData(key, value);
 	}
-	sort(karr.begin(), karr.end());
 	
-	for(int i=0; i<m; i++)
+	//명령 횟수 입력
+	while(m--)
 	{
 		int cmd, key, value;
-		scanf("%d", &cmd);
-		
-	//	cout<<"> "<<mData.count(0)<<'\n';
+		cin>>cmd>>key;
 		
 		if(cmd==1)
 		{
-			scanf("%d %d", &key, &value);
-			
-			karr.push_back(key);
-			sort(karr.begin(), karr.end());
-			
-			mData.insert(make_pair(key, value));
+			cin>>value;
+			AddData(key, value);
 		}
 		else if(cmd==2)
 		{
-			scanf("%d %d", &key, &value);
-			
-			printf("(%d)-", key);
-			
-			if(mData.count(key))
-			{
-				mData[key]=value;
-			}
-			else
-			{
-				viter iterPos1 = lower_bound(karr.begin(), karr.end(), key);
-				viter iterPos2 = lower_bound(karr.begin(), karr.end(), key-k);
-				
-			//	printf("iterPos1 : %d, iterPos2 : %d\n", *iterPos1, *iterPos2);
-				
-				
-				if(iterPos1 == karr.end() || abs(*iterPos1-key) > k) 
-				{
-					if(iterPos2 != karr.end() && abs(*iterPos2-key) <= k)
-						mData[*iterPos2] = value;
-				}
-					
-				else if(iterPos2 == karr.end() || abs(*iterPos2-key) > k) 
-				{
-					if(iterPos1 != karr.end() && abs(*iterPos1-key) <= k)
-						mData[*iterPos1] = value;
-				}
-				
-				else
-				{
-					if(abs(*iterPos1-key) >= abs(*iterPos2-key))
-						mData[*iterPos2] = value;
-					else
-						mData[*iterPos1] = value;
-				}
-			}
+			cin>>value;		
+			UpdateData(key, value);
 		}
 		else if(cmd==3)
-		{
-			scanf("%d", &key);
-			
-			
-			if(mData.count(key)==1)
-				printf("%d\n", mData[key]);
-			else 
-			{
-				viter iterPos1 = lower_bound(karr.begin(), karr.end(), key);
-				viter iterPos2 = lower_bound(karr.begin(), karr.end(), key-k);
-				
-				if(abs(*iterPos1-key) > k && abs(*iterPos2-key) > k)
-					printf("-1\n");
-				
-				else if(iterPos1 == karr.end() || abs(*iterPos1-key) > k) 
-				{
-					if(iterPos2 != karr.end() && abs(*iterPos2-key) <= k)
-						printf("%d\n", mData[*iterPos2]);
-				}
-					
-				else if(iterPos2 == karr.end() || abs(*iterPos2-key) > k) 
-				{
-					if(iterPos1 != karr.end() && abs(*iterPos1-key) <= k)
-						printf("%d\n", mData[*iterPos1]);
-				}
-				
-				else
-				{
-					if(abs(*iterPos1-key) == abs(*iterPos2-key) )
-						printf("?\n");
-				}
-			}
-			
-		}
+			PrtData(key);
 	}
 
 	
