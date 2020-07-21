c458f8767a7683962fd933fb1ac76040a98dd664
Oriburger <uss425@naver.com>
2020-07-21 23:45:50 +0900

    Create 9655_돌게임.cpp

---
+++ Sources/9655_돌게임.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin>>n;
+
+	if(n%2==1) cout<<"SK\n";
+	else cout<<"CY\n";
+
+	return 0;
+}
