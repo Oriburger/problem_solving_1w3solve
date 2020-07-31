6936f6c5913dd39b7356848d9a9c0487f0435609
Oriburger <uss425@naver.com>
2020-07-31 16:45:23 +0900

    Update 16236_아기상어.cpp

--- Sources/Undone/16236_아기상어.cpp
+++ Sources/Undone/16236_아기상어.cpp
@@ -1,28 +1,63 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <utility>
+#define INF 2147000000
 using namespace std;
 
 struct Pos{ int y; int x; };
 
+const int dy[4] = {-1,1,0,0};
+const int dx[4] = {0,0,-1,1};
+
 int n, board[21][21];
 vector<Pos> fish;
 Pos bsPos; //baby shark du dududududu
-int bsSize=2;
+int bsSize=2; //아기 상어의 크기
 
 //현재의 위치에서 가장 가까운 '먹을 수 있는' 물고기의 위치 리스트 반환
-vector<int> BFS(Pos start) 
+vector<Pos> BFS(Pos start) 
 {
-	queue<Pos> q;
+	vector<Pos> ret;
+	int dist = INF;
+	queue<pair<Pos, int> > q;
 	bool visited[21][21]={false};
 
 	visited[start.y][start.x]=true;
-	q.push(start);
+	q.push({start, 0});
 
 	while(!q.empty())
 	{
-		Pos curr = 
+		Pos curr = q.front().first;
+		int curDist = q.front().second;
+		q.pop();
+
+		cout<<curr.y<<','<<curr.x<<'\n';
+		
+		//아기상어가 먹을 수 있는 물고기라면,
+		if(board[curr.y][curr.x] < bsSize
+			&& board[curr.y][curr.x] >= 1 && board[curr.y][curr.x] <= 9)
+		{
+			ret.push_back({curr.y, curr.x});
+			dist = curDist;
+
+			cout<<dist<<'\n';
+		}
+
+		if(curDist <= dist)
+		for(int i=0; i<4; i++)
+		{
+			Pos next = {curr.y + dy[i], curr.x + dx[i]};
+
+			if(next.y<0 || next.x<0 || next.y>=n || next.x>=n) continue;
+			if(visited[next.y][next.x]) continue;
+
+			q.push({next, curDist+1});
+			visited[next.y][next.x]=true;
+		}
 	}
+
+	return ret;
 }
 
 int main()
@@ -42,7 +77,9 @@ int main()
 		}
 	}
 	
+	cout<<
 
+	BFS(bsPos);
 
 	return 0;
 }
