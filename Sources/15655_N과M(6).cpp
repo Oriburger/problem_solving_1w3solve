a9ef9e0945e8a57e23a8f1a8b298d41e7aa68878
Oriburger <uss425@naver.com>
2020-08-01 15:39:12 +0900

    Create 15655_N과M(6).cpp

---
+++ Sources/15655_N과M(6).cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> ans, arr;
+vector<bool> check(10001, false);
+	
+void Answer(int toPick, int lastPick)
+{
+	if(toPick==0)
+	{
+		for(auto p:ans)
+			cout<<p<<' ';
+		cout<<'\n';
+		return;
+	}
+	
+	for(int i=lastPick+1; i<n; i++)
+	{
+		if(check[arr[i]]) continue;
+
+		check[arr[i]]=true;
+		ans.push_back(arr[i]);
+
+		Answer(toPick-1, i);
+
+		ans.pop_back();
+		check[arr[i]]=false;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin>>n>>m;
+	
+	arr.resize(n, 0);
+	for(int i=0; i<n; i++)
+		cin>>arr[i];
+
+	sort(arr.begin(), arr.end());
+
+	Answer(m, -1);
+	
+	return 0;
+}
