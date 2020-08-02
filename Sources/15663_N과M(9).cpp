352d9213d5a6c4653db486566334025ad2b4626a
Oriburger <uss425@naver.com>
2020-08-02 11:06:45 +0900

    Create 15663_N과M(9).cpp

---
+++ Sources/15663_N과M(9).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int n, m;
+vector<int> arr, ans;
+set<int> check1;
+vector<bool> check2(10001, false);
+
+void Answer(int toPick, int picked)
+{	
+	if(toPick == 0)
+	{
+		if(check1.count(picked)!=0) return;
+		
+		check1.insert(picked);
+		
+		for(auto p : ans)
+			cout<<p<<' ';
+		cout<<'\n';
+		
+		return;
+	}
+		
+	for(int i=0; i<n; i++)
+	{
+		int next = picked*10+arr[i];
+		
+		if(check2[i]) continue;
+		
+		ans.push_back(arr[i]);
+		check2[i]=true;
+		
+		Answer(toPick-1, next);
+	
+		check2[i]=false;
+		ans.pop_back();
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin>>n>>m;
+	
+	arr.resize(n);
+	for(int i=0; i<n; i++)
+		cin>>arr[i];
+	sort(arr.begin(), arr.end());
+	
+	Answer(m, 0);
+	
+	return 0;
+}
