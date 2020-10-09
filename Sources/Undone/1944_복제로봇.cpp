977ff492d51aacb6724276d11cbfed47c6fea6f3
Oriburger <uss425@naver.com>
2020-10-09 13:57:43 +0900

    Update 1944_복제로봇.cpp

--- Sources/Undone/1944_복제로봇.cpp
+++ Sources/Undone/1944_복제로봇.cpp
@@ -40,13 +40,14 @@ struct DisjointSet
 int n, m, vCnt=0, eCnt=0;
 char board[MAX_N][MAX_N];
 int vID[MAX_N][MAX_N];
-bool visited[MAX_N][MAX_N];
 vector<Edge> edges;
 vector<Pos> stPos;
 
 void BFS(Pos start)
 {
 	queue<Edge> q;
+    bool visited[MAX_N][MAX_N];
+    
 	q.push({0, start.y, start.x});
 	visited[start.y][start.x]=true;
 
@@ -58,11 +59,12 @@ void BFS(Pos start)
 		for(int i=0; i<4; i++)
 		{
 			Edge next = {curr.cost+1, curr.y+dy[i], curr.x+dx[i]};
-
+            
 			if(visited[next.y][next.x]) continue;
 			if(board[next.y][next.x]=='1') continue;
 			if(next.y<0 || next.x<0 || next.y>=n || next.x>=n) continue;
-			if(board[next.y][next.x]=='K')
+
+			if(board[next.y][next.x]=='K' || board[next.y][next.x]=='S')
 			{
 				edges.push_back({next.cost, vID[curr.y][curr.x], vID[next.y][next.x]});
 				eCnt++; next.cost=0;
@@ -88,8 +90,8 @@ int main()
         }
 	}
 
-	for(const Pos &p : stPos)
-        BFS(p);
+	for(int i=0; i<stPos.size(); i++)
+        BFS(stPos[i]);
 
     sort(edges.begin(), edges.end(), CompEdge);
     
