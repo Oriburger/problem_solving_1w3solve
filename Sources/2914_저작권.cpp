4064af795c7867faf48a02c3296f8962b7d09edb
Oriburger <uss425@naver.com>
2020-12-25 15:50:48 +0900

    Create 2914_저작권.cpp

---
+++ Sources/2914_저작권.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, l;
+
+	cin>>a>>l;
+
+	cout<<a*l-a+1<<'\n';
+
+	return 0;
+}
