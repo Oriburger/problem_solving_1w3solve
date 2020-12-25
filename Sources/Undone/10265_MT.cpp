6d3cec0726df56ba3a74902fe1d26b198d67eb01
Oriburger <uss425@naver.com>
2020-12-25 15:44:42 +0900

    Update 10265_MT.cpp

--- Sources/Undone/10265_MT.cpp
+++ Sources/Undone/10265_MT.cpp
@@ -7,6 +7,7 @@ using namespace std;
 const int MAX_N = 1001;
 
 int n, k, visitCnt=1, sccCnt=0;
+vector<vector<int> > scc;
 vector<int> adj[MAX_N];
 vector<int> sccId(MAX_N), discovered(MAX_N);
 vector<int> indegree(MAX_N), sccSize(MAX_N);
@@ -28,13 +29,17 @@ int TarjanDFS(int curr)
 
 	if(lowLink == discovered[curr])
 	{
+		scc.push_back(vector<int>());
 		while(1)
 		{
 			int v = stk.top();
 			stk.pop();
-			finished[v]=true;
+
 			sccId[v]=sccCnt;
 			sccSize[sccCnt]++;
+			scc[sccCnt].push_back(v);
+
+			finished[v]=true;
 
 			if(v==curr) break;
 		}
@@ -64,7 +69,34 @@ int main()
 		if(sccId[i]!=sccId[i+1])
 			indegree[sccId[i]]++;
 
-	
+	vector<int> cache(sccCnt, 0);
+	vector<vector<bool> > visited;
+	vector<int> ans;
+
+	visited.resize(sccCnt, vector<bool>(sccCnt, false));
+
+	for(int i=0; i<sccCnt; i++)
+	{
+		if(indegree[i]==0) 
+			ans.push_back(i);
+
+		for(auto &p : scc[i])
+		{
+			for(auto &next : adj[p])
+			{
+				if(sccId[next]!=sccId[p] && !visited[sccId[next]][sccId[p]])
+				{
+					cache[sccId[next]] += sccSize[sccId[p]];
+					visited[sccId[next]][sccId[p]]=true;
+				}
+			}
+		}
+	}
+
+	for(auto &p : ans)
+	{
+		
+	}
 
 	return 0;
 }
