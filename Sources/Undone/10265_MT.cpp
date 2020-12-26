7c7b14242c0bee2d2aeafef593969d0678696175
Oriburger <uss425@naver.com>
2020-12-26 18:36:00 +0900

    Update 10265_MT.cpp

--- Sources/Undone/10265_MT.cpp
+++ Sources/Undone/10265_MT.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <queue>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 
 const int MAX_N = 1001;
 
 int n, k, visitCnt=1, sccCnt=0;
-vector<vector<int> > scc;
+int cache[1001][MAX_N];
+vector<int> maxSize(MAX_N), minSize(MAX_N);
 vector<int> adj[MAX_N];
+vector<vector<int> > scc;
 vector<int> sccId(MAX_N), discovered(MAX_N);
 vector<int> indegree(MAX_N), sccSize(MAX_N);
 vector<bool> finished(MAX_N);
 stack<int> stk;
+
+int GetAnswer(int k, int idx)
+{
+	if(idx == n) return 0;
+
+	int &ret = cache[k][idx];
+	if(ret!=-1) return ret;
+
+	ret = GetAnswer(k, idx+1);
+	
+	for(int i=minSize[idx]; i<=maxSize[idx]; i++)
+		if(k >= i)
+			ret = max(ret, GetAnswer(k-i, idx+1)+i);
+
+	return ret;
+}
 
 int TarjanDFS(int curr)
 {
@@ -48,6 +68,20 @@ int TarjanDFS(int curr)
 	return lowLink;
 }
 
+void Debug()
+{
+	for(int i=sccCnt-1; i>=0; i--)
+	{
+		cout<<"SccID "<<i;//<<"("<<sccSize[i]<<") :";
+		//for(auto &p : scc[i])
+		//	cout<<p<<", ";
+		
+		//cout<<" / indegree : "<<indegree[i]; 
+		//cout<<"\n";
+		cout<<" minSize : "<<minSize[i]<<", maxSize : "<<maxSize[i]<<'\n';
+	}
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -65,38 +99,41 @@ int main()
 		if(discovered[i]==0)
 			TarjanDFS(i);
 
-	for(int i=1; i<n; i++)
-		if(sccId[i]!=sccId[i+1])
-			indegree[sccId[i]]++;
-
-	vector<int> cache(sccCnt, 0);
-	vector<vector<bool> > visited;
-	vector<int> ans;
-
-	visited.resize(sccCnt, vector<bool>(sccCnt, false));
-
+	for(int i=sccCnt-1; i>=0; i--)
+	  for(auto &p : scc[i])
+	   	for(auto &next : adj[p])
+	   		if(sccId[p]!=sccId[next])
+	   			indegree[sccId[next]]++;
+	
 	for(int i=0; i<sccCnt; i++)
+		if(indegree[i]==0)
+			minSize[i]=maxSize[i]=sccSize[i];
+	
+	vector<bool> checked(sccCnt, false);
+	for(int i=sccCnt-1; i>=0; i--)
 	{
-		if(indegree[i]==0) 
-			ans.push_back(i);
-
 		for(auto &p : scc[i])
 		{
-			for(auto &next : adj[p])
+		   	for(auto &next : adj[p])
 			{
-				if(sccId[next]!=sccId[p] && !visited[sccId[next]][sccId[p]])
+		   		if(sccId[p]!=sccId[next] && !checked[sccId[next]])
 				{
-					cache[sccId[next]] += sccSize[sccId[p]];
-					visited[sccId[next]][sccId[p]]=true;
+					maxSize[sccId[next]] = sccSize[sccId[next]] + maxSize[sccId[p]];
+				//	minSize[]
+
+					checked[sccId[next]]=true;
 				}
 			}
 		}
 	}
+	
+	Debug();
 
-	for(auto &p : ans)
-	{
-		
-	}
+	for(int i=0; i<1001; i++)
+		memset(cache[i], -1, sizeof(int)*MAX_N);
+
+	cout<<GetAnswer(k, 0)<<'\n';
 
 	return 0;
 }
+
