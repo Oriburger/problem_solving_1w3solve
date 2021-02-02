319a33924c553ef739b160f036e514f059ae91f2
Oriburger <uss425@naver.com>
2021-02-02 20:31:53 +0900

    Update 13506_카멜레온부분문자열.cpp

--- Sources/13506_카멜레온부분문자열.cpp
+++ Sources/13506_카멜레온부분문자열.cpp
@@ -29,53 +29,36 @@ vector<int> GetPartialMatch(const string &s)
 	return pi;
 }
 
-int kmpSearch(const string &h, const string &n)
-{
-	if(h.size() < n.size()) return false;
-
-	int begin, matched, cnt=0;
-	begin = matched = 0;
-
-	while(begin <= h.size() - n.size())
-	{
-		if(matched < n.size() && h[begin+matched]==n[matched])
-		{
-			matched++;
-			if(matched==n.size()) cnt++;
-		}
-		else
-		{
-			if(matched==0) begin++;
-			else
-			{
-				begin += matched - pi[matched-1];
-				matched = pi[matched-1];
-			}
-		}
-	}
-	return cnt;
-}
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	string s; int ans=0;
+	string s;
+	int ans=0, temp;
+	bool flag = false;
+
 	cin>>s;
 
 	vector<int> pi = GetPartialMatch(s);
+	temp = pi[s.size()-1];
 
-	for(int i=1; i<=pi[s.size()-1]; i++)
-	{
-		int temp = kmpSearch(s, s.substr(0, i));
-		if(temp < 3) continue;
-		ans = max(ans, i);
+	while(temp)
+	{		
+		for(int i=1; i<s.size()-1; i++)
+		{
+			if(temp == pi[i])
+			{
+				cout<<s.substr(0, temp)<<'\n';
+				flag = true;
+				break;
+			}
+		}
+		if(flag) break;
+		temp = pi[temp-1];
 	}
 
-	if(ans == 0) cout<<-1<<'\n';
-	else 
-		cout<<s.substr(0, ans)<<'\n';
+	if(!flag) cout<<-1<<'\n';
 
 	return 0;
 }
