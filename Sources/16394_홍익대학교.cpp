20045ac6442e06634d0a4751c4713aec1cb15221
Oriburger <uss425@naver.com>
2021-02-13 16:57:42 +0900

    Create 16394_홍익대학교.cpp

---
+++ Sources/16394_홍익대학교.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n;
+    
+    cin>>n;
+    cout<<n-1946<<'\n';
+    
+    return 0;
+} 
