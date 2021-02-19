d2b3b118d922fb891ab4ec561569443bcb612cea
Oriburger <uss425@naver.com>
2021-02-19 23:51:05 +0900

    Update 9376_탈옥.cpp

--- Sources/Undone/9376_탈옥.cpp
+++ Sources/Undone/9376_탈옥.cpp
@@ -1,91 +1,103 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 #include <utility>
 #include <algorithm>
-#define INF 2147000000
 using namespace std;
 
-struct Pos { int y; int x; };
+const int dy[4] = {1, -1, 0, 0};
+const int dx[4] = {0, 0, 1, -1};
+const int INF = 2147000000;
 
-const int dy[4]={1,-1,0,0};
-const int dx[4]={0,0,1,-1};
+struct Pos{ int y, x; };
 
-int h, w;
-char board[100][100];
-vector<Pos> exitArr, prisoner;
+int t, h, w, ans;
+vector<char> board[103];
+vector<Pos> prisoner_pos;
+vector<int> cache[103];
 
-int BFS(vector<Pos> start)
+void BFS(Pos start)
 {
-	queue<pair<Pos, int> > q;
-	int visited[100][100]={0};
-	
-	for(int i=0; i<start.size(); i++)
-	{
-		q.push(start[i]);
-		visited[start[i].y][start[i].x]=true;	
-	}
-	
+	queue<Pos> q;
+	q.push(start);
+	cache[start.y][start.x] = 0;
+
 	while(!q.empty())
 	{
-		pair<Pos, int> curr = q.front();
+		Pos curr = q.front();
 		q.pop();
-		
+
+		//cout<<curr.y<<' '<<curr.x<<'\n';
+
 		for(int i=0; i<4; i++)
 		{
-			int ny = curr.first.y + dy[i];
-			int nx = curr.first.x + dx[i];
+			Pos next = {curr.y + dy[i], curr.x + dx[i]};
 			
-			if(ny<0 || nx<0 || ny>=h || nx>=w) continue;
-			if(visited[ny][nx]) continue;
-			if(board[ny][nx]=='*') continue;
+			if(next.y < 0 || next.x < 0 
+				|| next.y >= h+1 || next.x >= w+1) continue;
+			if(board[next.y][next.x]=='*') continue;
+			if(cache[next.y][next.x]!=INF) continue;
+
+			cache[next.y][next.x]=cache[curr.y][curr.x];
+			if(board[next.y][next.x]=='#')
+			{
+				board[next.y][next.x]='.';
+				cache[next.y][next.x]=cache[curr.y][curr.x] + 1;
+			}
 			
-			visited[ny][nx] = true;
-			
-			if(board[ny][nx]=='#') 
-				q.push({{ny, nx}, curr.second+1});
-			else 
-				q.push({{ny, nx}, curr.second});
+			q.push(next);
 		}
 	}
-	
-	return INF; 
 }
 
 int main()
-{	
+{
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int t; cin>>t;
-	
+	cin>>t;
+
 	while(t--)
-	{		
+	{
 		cin>>h>>w;
-		
-		for(int i=0; i<h; i++)
+
+		ans=0;
+		prisoner_pos.clear();
+		for(int i=0; i<h+2; i++)
 		{
-			for(int j=0; j<w; j++)
+			board[i].clear();
+			cache[i].clear();
+			board[i].resize(w+2, '.');
+			cache[i].resize(w+2, INF);
+		}
+	
+		for(int i=1; i<=h; i++)
+			for(int j=1; j<=w; j++)
 			{
 				cin>>board[i][j];
-				if(board[i][j]=='#' || board[i][j]=='.')
-					if(i==0 || j==0 || i==h-1 || j==w-1)
-						exitArr.push_back({i, j});
 				if(board[i][j]=='$')
-					prisoner.push_back({i, j});
+					prisoner_pos.push_back({i, j});
 			}
+
+		BFS({0, 0});
+
+		for(int i=0; i<=h+1; i++)
+		{
+			for(int j=0; j<=w+1; j++)
+			{
+				if(cache[i][j]==INF) cout<<"@";
+				else cout<<cache[i][j];
+			}
+			cout<<'\n';
 		}
-		
-		int ansA = BFS({prisonerArr[0]});
-		int ansB = BFS({prisonerArr[1]});
-		int ansAB = BFS({prisonerArr[0], prisonerArr[1]});
-		
-		cout<<aAns+bAns<<'\n';
-		
-		prisoner.clear();
-		exitArr.clear();
-		cout<<"\n";
+		cout<<"----\n";
+
+		for(Pos &p : prisoner_pos)
+			ans += cache[p.y][p.x];
+
+		cout<<ans<<'\n';
 	}
-	
+
 	return 0;
-}	
+}
