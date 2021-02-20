6caf53ba6a0aa054bfa89ae10b9a853d16c0f394
Oriburger <uss425@naver.com>
2021-02-20 16:45:55 +0900

    Update 9376_탈옥.cpp

--- Sources/Undone/9376_탈옥.cpp
+++ Sources/Undone/9376_탈옥.cpp
@@ -1,18 +1,7 @@
-/* ------------------------------
-
-1. 맵 바깥 경계를 만듦
-2. 죄수의 각 위치에서 BFS
-3. BFS 수행때, 열게된 문은 다음 BFS에서 열린채로 유지
-4. 각각의 최소 횟수를 더해서 출력
-
-
- 현재? 구현중 (ing)
-
------------------------------- */
-
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <deque>
 #include <cstring>
 #include <utility>
 #include <algorithm>
@@ -27,20 +16,23 @@ struct Pos{ int y, x; };
 int t, h, w, ans;
 vector<char> board[103];
 vector<Pos> prisoner_pos;
-vector<int> cache[103];
+vector<int> sum[103];
 
 void BFS(Pos start)
 {
-	queue<Pos> q;
-	q.push(start);
+	deque<Pos> q;
+	q.push_back(start);
+	vector<int> cache[103];
+
+	for(int i=0; i<h+2; i++)
+		cache[i].resize(w+2, INF);
+
 	cache[start.y][start.x] = 0;
 
 	while(!q.empty())
 	{
 		Pos curr = q.front();
-		q.pop();
-
-		//cout<<curr.y<<' '<<curr.x<<'\n';
+		q.pop_front();
 
 		for(int i=0; i<4; i++)
 		{
@@ -51,16 +43,32 @@ void BFS(Pos start)
 			if(board[next.y][next.x]=='*') continue;
 			if(cache[next.y][next.x]!=INF) continue;
 
-			cache[next.y][next.x]=cache[curr.y][curr.x];
+			cache[next.y][next.x] = cache[curr.y][curr.x];
 			if(board[next.y][next.x]=='#')
 			{
-				board[next.y][next.x]='.';
-				cache[next.y][next.x]=cache[curr.y][curr.x] + 1;
+				board[next.y][next.x] = '.';
+				cache[next.y][next.x] = cache[curr.y][curr.x] + 1;
+				q.push_front(next);
 			}
-			
-			q.push(next);
+			else q.push_back(next);
 		}
 	}
+	
+	for(int i=0; i<h+2; i++)
+	{
+		for(int j=0; j<w+2; j++)
+		{
+			if(cache[i][j]==INF)
+			{
+				cout<<"@";
+				continue; 
+			}
+			cout<<cache[i][j];
+			sum[i][j]+=cache[i][j];
+		}
+		cout<<'\n';
+	}
+	cout<<"------\n";
 }
 
 int main()
@@ -79,9 +87,9 @@ int main()
 		for(int i=0; i<h+2; i++)
 		{
 			board[i].clear();
-			cache[i].clear();
+			sum[i].clear();
 			board[i].resize(w+2, '.');
-			cache[i].resize(w+2, INF);
+			sum[i].resize(w+2, INF);
 		}
 	
 		for(int i=1; i<=h; i++)
@@ -92,31 +100,25 @@ int main()
 					prisoner_pos.push_back({i, j});
 			}
 
+		prisoner_pos.push_back({0, 0});
 		for(Pos &p : prisoner_pos)
-		{
 			BFS({p.y, p.x});
-			ans += cache[0][0];
-			for(int i=0; i<h+2; i++)
-			{
-				cache[i].clear();
-				cache[i].resize(w+2, INF);
-			}
-		}
 
-/*
+
 		for(int i=0; i<=h+1; i++)
 		{
 			for(int j=0; j<=w+1; j++)
 			{
-				if(cache[i][j]==INF) cout<<"@";
-				else cout<<cache[i][j];
+			//	if(cache[i][j]==INF) cout<<"@";
+			//	else cout<<cache[i][j];
+				ans = min(ans, sum[i][j]);
 			}
-			cout<<'\n';
 		}
-		cout<<"----\n";
-*/
+		//cout<<"----\n";
+
 
 		cout<<ans<<'\n';
+		cout<<"===================\n";
 	}
 
 	return 0;
