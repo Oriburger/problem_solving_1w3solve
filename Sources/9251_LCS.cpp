e76739143c2bc7ffd3edbf7a33eacd835347243f
Oriburger <uss425@naver.com>
2021-02-24 22:50:49 +0900

    Update 9251_LCS.cpp

--- Sources/9251_LCS.cpp
+++ Sources/9251_LCS.cpp
@@ -2,33 +2,30 @@
 #include <string>
 using namespace std;
 
+const int MAX = 1001;
+int cache[MAX][MAX];
 string s1, s2;
-int dp[4001][4001];
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 
 	cin>>s1>>s2;
 
-	s1.insert(s1.begin(), '.');
-	s2.insert(s2.begin(), '.');
+	s1 = ' ' + s1;
+	s2 = ' ' + s2;
 
-	for(int i=0; i<s1.size(); i++)
+	for(int i=1; i<s1.size(); i++)
 	{
-		for(int j=0; j<s2.size(); j++)
+		for(int j=1; j<s2.size(); j++)
 		{
-			if(i==0 || j==0) dp[i][j]=0;
-
-			else if(s1[i]==s2[j])
-				dp[i][j] = dp[i-1][j-1] + 1;
-			else
-				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+			if(s1[i]==s2[j]) cache[i][j] = cache[i-1][j-1]+1;
+			else cache[i][j] = max(cache[i][j-1], cache[i-1][j]);
 		}
 	}
-
-	cout<<dp[s1.size()-1][s2.size()-1]<<'\n';
+	
+	cout<<cache[s1.size()-1][s2.size()-1]<<'\n';
 
 	return 0;
 }
