84fba65080a11b0e09479cb97f8f1dfc0da4a223
Oriburger <uss425@naver.com>
2021-03-13 16:18:18 +0900

    Update 1562_계단수.cpp

--- Sources/1562_계단수.cpp
+++ Sources/1562_계단수.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 const int MAX = 10;
 const int INF = 1000000000;
-const int d[2] = {-1, 1};
+const int d[2] = {-1, 1}; //탐색 시 curr이 1씩 증감
 
-int cache[101][MAX][(1<<MAX)];
 int ans = INF;
+int cache[101][MAX][(1<<MAX)]; //﻿남은 자릿수 k, 수들의 picked 여부를 나타내는 state,
+                               //현재 pick한 수 curr이 주어졌을때, 계단수의 개수
 
 int Answer(int k, int curr, int state)
 {
-	if(k==0)
+	if(k==0) //수를 다 골랐다면?
 	{
-		if(state!=(1<<(MAX+1))-1) return -1;
-		return 1;
+		if(state!=(1<<(MAX+1))-1) return -1; //0~9가 다 포함되지 않았다면 -1반환
+		return 1; //조건에 맞는 계단수라면?
 	}
 
 	int &ret = cache[k][curr][state];
@@ -22,10 +23,12 @@ int Answer(int k, int curr, int state)
 
 	for(int i=0; i<2; i++)
 	{
-		int next = curr + d[i];
-		if(next<0 || next>=MAX) continue;
+		int next = curr + d[i]; 
+		if(next<0 || next>=MAX) continue; //범위(0~9)에 벗어나는 수라면
 
-		int nextVal = Answer(k-1, next, (state | (1<<next)))%INF;
+        //OR연산으로 next자리의 비트를 켜주기 때문에,
+        //이미 해당 자리의 비트가 켜져 있어도 상관없다. 
+		int nextVal = Answer(k-1, next, (state | (1<<next)))%INF; 
 		if(nextVal != -1) 
 			ret = (ret + nextVal)%INF;
 	}
