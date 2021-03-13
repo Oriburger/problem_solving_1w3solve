f0fac8bfe632897b3879fcde1734aa22ac7afa86
Oriburger <uss425@naver.com>
2021-03-13 15:04:36 +0900

    Update 2098_외판원순회.cpp

--- Sources/2098_외판원순회.cpp
+++ Sources/2098_외판원순회.cpp
@@ -10,25 +10,25 @@ int n, w[MAX_N][MAX_N];
 int dp[MAX_N][1<<MAX_N];
 int ans = INF;
 
-int Answer(int curr, int state)
+int Answer(int curr, int state) //현재 정점이 curr이고 state라는 상태일때, 최소 cost 
 {
-	if(state == (1<<n)-1 )
+	if(state == (1<<n)-1 ) //모든 도시를 다 방문한 상태라면
 	{
-		if(w[curr][0]==0) return INF;
-		return w[curr][0];
+		if(w[curr][0]==0) return INF; //연결이 되지 않은 도시라면 INF 반환 
+		return w[curr][0]; //연결이 된 도시라면, 다시 시작점으로 돌아가는 cost 반환 
 	}
 
-	int& ret = dp[curr][state];
+	int& ret = dp[curr][state]; 
 	if(ret != 0) return ret;
 
-	int tmp = INF;
+	int tmp = INF; 
 	for(int next=0; next<n; next++)
 	{
+        //연결이 되어있지 않거나, 이미 방문한 정점이라면
 		if(w[curr][next]==0  || state & (1<<next)) continue;
-
+        
 		int nextVal = Answer(next, state | (1<<next));
-
-		if(nextVal!=INF)
+		if(nextVal!=INF) //nextVal이 INF가 아니라면 tmp 갱신
 			tmp = min(tmp, w[curr][next] + nextVal);
 	}
 
@@ -38,7 +38,7 @@ int Answer(int curr, int state)
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL); 
 
 	cin>>n;
 
