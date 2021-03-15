4696a3c2d58c0786571ebaee86c5a3373ce22a95
Oriburger <uss425@naver.com>
2021-03-15 21:50:53 +0900

    Update 4991_로봇청소기.cpp

--- Sources/4991_로봇청소기.cpp
+++ Sources/4991_로봇청소기.cpp
@@ -10,43 +10,44 @@ struct Pos{ int y; int x; };
 const int dy[4] = {1, -1, 0, 0};
 const int dx[4] = {0, 0, -1, 1};
 
-Pos stPos;
-int w, h, dirtCnt;
-char board[20][20];
-int visited[20][20][(1<<10)];
+Pos stPos; //시작 좌표
+int w, h, dirtCnt; //가로, 세로, 더러운 칸 개수
+char board[20][20]; //직사각형 모양의 방
+int visited[20][20][(1<<10)]; //더러운 칸은 최대 10개 이므로, (1<<10)만큼 할당
 
 int BFS(Pos start)
 {
-	queue<pair<Pos, int> > q;
-	q.push({start, 0});
-	visited[start.y][start.x][0] = 0;
+	queue<pair<Pos, int> > q; //좌표에 따라, 상태에 따라 Queue에 push
+	q.push({start, 0}); //시작좌표 push, 초기 상태는 0
+	visited[start.y][start.x][0] = 0; //초기의 이동 횟수 0
 
-	while(!q.empty())
+	while(!q.empty()) 
 	{
-		Pos curr = q.front().first;
-		int curState = q.front().second;
+		Pos curr = q.front().first; //현재 좌표 
+		int curState = q.front().second; //현재 상태
 		q.pop();
 
-		if(curState == ((1<<dirtCnt)-1))
-			return visited[curr.y][curr.x][curState];
+		if(curState == ((1<<dirtCnt)-1)) //만약, 방에 있는 먼지들을 모두 청소했다면
+			return visited[curr.y][curr.x][curState]; //이동 횟수 반환
 
-		for(int i=0; i<4; i++)
+		for(int i=0; i<4; i++) //상, 하, 좌, 우 탐색
 		{
-			Pos next = {curr.y + dy[i], curr.x + dx[i]};
-			int nextState = curState;
+			Pos next = {curr.y + dy[i], curr.x + dx[i]}; //다음 좌표
+			int nextState = curState; //다음 상태
 
-			if(next.y<0 || next.x<0 || next.y>=h || next.x>=w) continue;
-			if(board[next.y][next.x]=='x') continue;
-			if(board[next.y][next.x]>='0' && board[next.y][next.x]<='9')
-				nextState |= (1<<(board[next.y][next.x]-'0'));
+			if(next.y<0 || next.x<0 || next.y>=h || next.x>=w) continue; //out of range
+			if(board[next.y][next.x]=='x') continue; //다음 칸이 가구라면 
+			if(board[next.y][next.x]>='0' && board[next.y][next.x]<='9') //다음 칸이 먼지라면
+				nextState |= (1<<(board[next.y][next.x]-'0')); //nextState 갱신(해당 비트 on)
 			
-			if(visited[next.y][next.x][nextState]) continue;
+			if(visited[next.y][next.x][nextState]) continue; //다음 좌표&상태가 이미 방문한 상태라면 continue
 
-			visited[next.y][next.x][nextState] = visited[curr.y][curr.x][curState] + 1;
+			//다음 좌표&상태 이동횟수 갱신 및 Queue에 push
+			visited[next.y][next.x][nextState] = visited[curr.y][curr.x][curState] + 1; 
 			q.push({next, nextState});
 		}	
 	}
-	return -1;
+	return -1; //모든 먼지를 치울 수 없다면 -1 반환
 }
 
 int main()
@@ -69,8 +70,8 @@ int main()
 			{
 				cin>>board[i][j];
 				if(board[i][j]=='o') stPos = {i, j};
-				else if(board[i][j]=='*')
-					board[i][j]=(dirtCnt++)+'0';
+				else if(board[i][j]=='*') //먼지가 있는 칸을 숫자로 매겨준다. -> 비트연산 위함
+					board[i][j]=(dirtCnt++)+'0'; 
 			}
 		}
 
