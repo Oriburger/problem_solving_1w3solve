52199a55b59e99f2d9a42e32044b8fe885a675d7
Jaeho Lee <uss425@naver.com>
2021-03-17 18:47:11 +0900

    Update and rename Sources/Undone/1708_볼록껍질.cpp to Sources/1708_볼록껍질.cpp

---
+++ Sources/1708_볼록껍질.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cmath>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 100000;
+typedef long long ll;
+
+struct Point
+{
+	int x, y; //실제 위치
+	int p, q; //기준점으로부터 상대 위치
+
+	Point(int x1=0, int y1=0): x(x1), y(y1), p(1), q(0){}
+};
+
+bool CompPoint(const Point &A, const Point &B)
+{
+	if(1LL * A.q * B.p != 1LL * A.p * B.q)
+		return 1LL * A.q * B.p < 1LL * A.p * B.q;
+
+	if(A.y != B.y) return A.y < B.y;
+
+	return A.x < B.x;
+}
+
+ll CCW(const Point &A, const Point &B, const Point &C)
+{
+	return 1LL*(B.x-A.x)*(C.y-A.y) - 1LL*(B.y-A.y)*(C.x-A.x);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	vector<Point> p;
+
+	cin>>n;
+	p.resize(n);
+	for(int i=0; i<n; i++)
+	{
+		int x, y;
+		cin>>x>>y;
+		p[i] = Point(x, y);
+	}
+
+	//점들을 y -> x좌표 순으로 정렬 : 0번 점이 왼쪽 제일 아래에 위치
+	sort(p.begin(), p.end(), CompPoint);
+	for(int i=1; i<n; i++)
+	{
+		p[i].p = p[i].x - p[0].x;
+		p[i].q = p[i].y - p[0].y;
+	}
+	//0번을 제외한 점들을 반시계 방향으로 정렬
+	sort(p.begin()+1, p.end(), CompPoint);
+
+	stack<int> stk;
+	//스택에 0, 1번 점을 넣음
+	stk.push(0);
+	stk.push(1);
+	int next = 2;
+	//모든 점을 훑는다
+	while(next<n)
+	{
+		while(stk.size() >= 2)
+		{
+			int first, second;
+			first=stk.top();
+			stk.pop();
+			second=stk.top();
+			if(CCW(p[second], p[first], p[next])>0)
+			{
+				stk.push(first);
+				break;
+			}
+		}
+		stk.push(next++);
+	}
+
+	cout<<stk.size()<<'\n';
+
+	return 0;
+}
