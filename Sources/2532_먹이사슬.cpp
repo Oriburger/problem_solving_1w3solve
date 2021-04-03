a66c2819f0913169d7f843567cbc5740dc3fa7b2
Jaeho Lee <uss425@naver.com>
2021-04-03 16:53:43 +0900

    Update 2532_먹이사슬.cpp

--- Sources/2532_먹이사슬.cpp
+++ Sources/2532_먹이사슬.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 typedef vector<int>::iterator ITERPOS;
 typedef pair<int, int> P;
-const int MAX = 500000;
 
 int n;
 vector<P> arr;
 vector<int> temp;
 
-bool comp(const P& p, const P& q)
+bool comp(const P& p, const P& q) 
 {
-	if(p.first == q.first) return p.second > q.second;
+	if(p.first == q.first) //LIS에 유리하도록 배치
+        return p.second > q.second;
 	return p.first < q.first;
 }
 
@@ -31,22 +31,22 @@ int main()
 		cin>>idx>>l>>r;
 		arr.push_back({l, r});
 	}
-	sort(arr.begin(), arr.end(), comp);
-	arr.erase(unique(arr.begin(), arr.end()), arr.end());
+	sort(arr.begin(), arr.end(), comp); //L값에 따라 오름차순 정렬 
+	arr.erase(unique(arr.begin(), arr.end()), arr.end()); //중복 제거!!!
 
-	temp.push_back(-1);
+	temp.push_back(-1); //이분탐색을 이용한 LIS 길이 찾기 
 	for(int i=n-1; i>=0; i--)
 	{
-		if(temp.back() <= arr[i].second)
+		if(temp.back() <= arr[i].second) //크거나 같다면? temp에 push
 			temp.push_back(arr[i].second);
-		else
-		{
+		else //작다면? upper_bound 위치를 찾아서 교체
+		{ 
 			ITERPOS iter = upper_bound(temp.begin(), temp.end(), arr[i].second);
 			*iter = arr[i].second;
 		}
 	}	
 
-	cout<<temp.size()-1<<'\n';
+	cout<<temp.size()-1<<'\n'; //LIS길이 출력
 
 	return 0;
 }
