301f95d35b85c2b3297652cc19d2065a6e8cd889
Jaeho Lee <uss425@naver.com>
2021-04-12 23:31:58 +0900

    Update 9576_책_나눠주기.cpp

--- Sources/9576_책_나눠주기.cpp
+++ Sources/9576_책_나눠주기.cpp
@@ -10,7 +10,7 @@ int t, n, m;
 vector<P> arr; 
 vector<bool> check;
 
-bool comp(P& p, P& q)
+bool comp(P& p, P& q) //비교함수
 {
 	return p.second != q.second ? p.second < q.second 
                                 : p.first < q.first;
@@ -35,12 +35,13 @@ int main()
 			cin>>a>>b;
 			arr.push_back({a, b});
 		}
-		sort(arr.begin(), arr.end(), comp);
+		sort(arr.begin(), arr.end(), comp); //끝 번호를 기준으로 오름차순 정렬 
 
 		int ans = 0;
-		for(int i=0; i<m; i++)
+		for(int i=0; i<m; i++) //정렬된 신청서를 순회 
 		{
-			for(int j=arr[i].first; j<=arr[i].second; j++)
+            //시작번호 ~ 끝번호에서 아직 남아있는 책을 준다.
+			for(int j=arr[i].first; j<=arr[i].second; j++) 
 			{
 				if(!check[j])
 				{
