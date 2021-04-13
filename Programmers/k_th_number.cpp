ddb134c0185ca9467a7a0e21a5720181d6a62809
Jaeho Lee <uss425@naver.com>
2021-04-13 18:56:36 +0900

    Create k_th_number.cpp

---
+++ Programmers/k_th_number.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) 
+{
+    vector<int> answer;
+    
+    for(int i=0; i<commands.size(); i++)   
+    {
+        int p = commands[i][0];
+        int q = commands[i][1];
+        int k = commands[i][2];ㄴㄴ
+        
+        vector<int> temp;
+        for(int j=p-1; j<q; j++)
+            temp.push_back(array[j]);
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[k-1]);
+    }
+    
+    return answer;
+}
