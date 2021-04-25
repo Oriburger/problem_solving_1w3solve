538cf02bbde480d9cf5f47f603bf3bed19e88e67
Jaeho Lee <uss425@naver.com>
2021-04-25 11:29:28 +0900

    Update and rename Programmers/Undone/Biggest.cpp to Programmers/Biggest.cpp

--- Programmers/Biggest.cpp
+++ Programmers/Biggest.cpp
@@ -7,16 +7,14 @@ vector<string> arr;
 
 bool comp(string &a, string &b)
 {
-    for(int i=0; i<min(a.size(), b.size()); i++)
-    {
-        if(a[i] > b[i]) return true;
-        else if(a[i] < b[i]) return false;
-    }
+    const int n1 = stoi(a+b);
+    const int n2 = stoi(b+a);
+    return n1 > n2;
 }
 
 string solution(vector<int> num)
 {
-    string ret; 
+    string tmp, ret;
     
     for(int i=0; i<num.size(); i++)
         arr.push_back(to_string(num[i]));
@@ -24,7 +22,16 @@ string solution(vector<int> num)
     sort(arr.begin(), arr.end(), comp);
     
     for(int i=0; i<num.size(); i++)
-        ret += arr[i];
+        tmp = tmp + arr[i];
     
-    return to_string(stoi(ret));
+    bool flag = false;
+    for(int i=0; i<tmp.size(); i++)
+    {
+        if(!flag && tmp[i]=='0') continue;
+        ret.push_back(tmp[i]);
+        flag = true;
+    }
+    if(ret=="") ret = "0";
+    
+    return ret;
 }
