8e0c6021b06233912b58ff21d04d49eb907200c2
Jaeho Lee <uss425@naver.com>
2021-05-08 15:59:45 +0900

    Update Carpet.cpp

--- Programmers/Carpet.cpp
+++ Programmers/Carpet.cpp
@@ -8,19 +8,19 @@ vector<int> solution(int brown, int yellow)
     vector<int> answer;
     
     bool flag = false;
-    for(int i=3; i<=brown; i++)
+    for(int i=3; i<=brown; i++) //적당히 가로 범위 잡아주고
     {
-        for(int j=3; j<=i; j++)
+        for(int j=3; j<=i; j++) //세로는 가로보다 짧게
         {
-            if(i*j - yellow == brown
+            if(i*j - yellow == brown //카펫 모양이 만들어진다면?
               && (i-2)*(j-2) == yellow)
             {
-                answer.push_back(i);
+                answer.push_back(i); //가로, 세로 순서대로 push
                 answer.push_back(j);
                 flag = true;
-                break;
+                break; 
             }
-            else if(i*j - yellow > brown)
+            else if(i*j - yellow > brown) 
                 break;
         }
         if(flag) break;
