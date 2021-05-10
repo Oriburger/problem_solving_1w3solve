fae2fe5bb8716d26590fcc2dad4239831f8fb7ec
Jaeho Lee <uss425@naver.com>
2021-05-10 19:02:03 +0900

    Update Printer.cpp

--- Programmers/Printer.cpp
+++ Programmers/Printer.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
-#include <deque>
+#include <deque> //반복자 사용을 위해 deque 사용
 #include <utility>
 using namespace std;
 
@@ -8,11 +8,11 @@ typedef pair<int, int> P;
 
 int solution(vector<int> priorities, int location)
 {
-    deque<P> q;
+    deque<P> q; //{문서의 번호, 중요도}
     int answer = 0;
     const int n = priorities.size();
     
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++) //초기 큐 구성
         q.push_back({i, priorities[i]});
     
     while(1)
@@ -21,19 +21,19 @@ int solution(vector<int> priorities, int location)
         deque<P>::iterator iter;
         
         bool flag = false;
-        for(iter = q.begin()+1; iter!= q.end(); iter++)
+        for(iter = q.begin()+1; iter!= q.end(); iter++) 
         {
-            if(iter->second > curr.second)
+            if(iter->second > curr.second) //큐에 중요도가 더 높은 문서가 있다면?
             {
                 flag = true;
                 break;
             }
         }
-        if(flag) q.push_back(curr);
-        else answer+=1;
+        if(flag) q.push_back(curr); //맨 뒤에 넣는다.
+        else answer+=1; //없다면 순서를 카운팅 해주고 pop
         q.pop_front();
         
-        if(!flag && curr.first == location) break;
+        if(!flag && curr.first == location) break; //정답을 찾았다면
     }
     
     return answer;
