5bf27e94e6099139665be381ef1b14895708f439
Jaeho Lee <uss425@naver.com>
2021-05-11 23:59:33 +0900

    Update TruckCrossing.cpp

--- Programmers/TruckCrossing.cpp
+++ Programmers/TruckCrossing.cpp
@@ -1,58 +1,3 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
 #include <string>
 #include <vector>
 #include <queue>
@@ -89,7 +34,6 @@ int solution(const int bridge_length, const int weight, vector<int> truck_weight
                 bridge.pop();
                 check[i] = true;
                 answer++;
-                //prt_queue(bridge);
             }
         }
 
@@ -101,8 +45,6 @@ int solution(const int bridge_length, const int weight, vector<int> truck_weight
         }
         bridge.pop();
         answer++;
-
-        //prt_queue(bridge);
     }
 
     return answer;
