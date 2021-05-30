65acf19da4f95a4ebe17c59c330c33cdb422076f
Jaeho Lee <uss425@naver.com>
2021-05-30 14:25:11 +0900

    Update DualPriorityQueue.cpp

--- Programmers/DualPriorityQueue.cpp
+++ Programmers/DualPriorityQueue.cpp
@@ -10,25 +10,25 @@ vector<int> solution(vector<string> operations)
 {
     deque<int> answer;
     
-    for(const string &p : operations) 
+    for(const string &p : operations) //연산을 하나씩 수행
     {
         dIter iter;
-        if(p[0]=='I')
+        if(p[0]=='I') //삽입 연산이라면? 
         {
-            int num = stoi(p.substr(2));
-            iter = lower_bound(answer.begin(), answer.end(), num);
-            answer.insert(iter, num);
+            int num = stoi(p.substr(2)); //숫자를 stoi와 substr을 활용해 추출
+            iter = lower_bound(answer.begin(), answer.end(), num); //이분탐색으로 들어갈 자리를 탐색
+            answer.insert(iter, num); //해당 자리에 insert
         }
-        else if(p[0]=='D')
+        else if(p[0]=='D') //삭제 연산이라면? 
         {
-            if(!answer.size()) continue;
+            if(!answer.size()) continue; //만약, answer이 비었다면 무시
             
-            if(p[2]=='-') answer.pop_front();
-            else answer.pop_back();
+            if(p[2]=='-') answer.pop_front(); //최솟값을 pop
+            else answer.pop_back(); //최댓값을 pop
         }
     }
     
-    if(!answer.size()) return {0, 0};
+    if(!answer.size()) return {0, 0}; //답이 없다면 0,0을 반환
     
-    return {answer.back(), answer.front()};
+    return {answer.back(), answer.front()}; //최대, 최소를 반환
 }
