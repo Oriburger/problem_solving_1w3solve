8f85b0b1b814701375c4ef02ff3945836986bc98
Jaeho Lee <uss425@naver.com>
2021-08-09 19:44:35 +0900

    Update 9764_서로다른자연수의합.cpp

--- Sources/9764_서로다른자연수의합.cpp
+++ Sources/9764_서로다른자연수의합.cpp
@@ -6,14 +6,15 @@ const int INF = 100999;
 int n;
 int cache[2001][2001];
 
+//k를 0으로 만드는 방법의 수 (마지막으로 사용한 수가 curr)
 int GetAnswer(int k, int curr)
 {
-	if(k==0) return 1;
+	if(k==0) return 1; //기저사례 : 0을 만들 수 있다면, 방법을 하나 찾음
 
 	int &ret = cache[k][curr];
-	if(ret != 0) return ret;
+	if(ret != 0) return ret; //메모이제이션
 
-	for(int i=curr+1; i<=k; i++)
+	for(int i=curr+1; i<=k; i++) //중복 사용 방지 : 마지막으로 사용한 수+1 ~ k를 이용 
 		ret = (GetAnswer(k-i, i)+ret)%INF;
 
 	return ret%INF;
