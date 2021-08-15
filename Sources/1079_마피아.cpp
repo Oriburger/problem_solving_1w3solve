4364bb530e82dac02e0d93c4ccc326a452b0c4e9
Jaeho Lee <uss425@naver.com>
2021-08-15 21:27:37 +0900

    Update 1079_마피아.cpp

--- Sources/1079_마피아.cpp
+++ Sources/1079_마피아.cpp
@@ -18,44 +18,44 @@ inline bool isDead(int idx)
 int Solve(int cnt)
 {
 	if(flag) return 0;
-	if(isDead(mafia) || cnt==1)
+	if(isDead(mafia) || cnt==1) //마피아 혼자 살아남거나, 마피아가 죽은 경우라면 반환
 	{
-		flag = (cnt==1);
+		flag = (cnt==1); //만약, 마피아 혼자 남는다면? 다른 모든 경우는 고려하지 않음
 		return 0;
 	}
 	
 	int ret = 0;
 	
-	if(cnt%2) //짝수 : 낮
+	if(cnt%2) //짝수 : 낮의 경우
 	{
 		int g_val=-1, g_idx=-1;
-		for(int i=0; i<n; i++)
+		for(int i=0; i<n; i++) //최대 유죄 지수인 사람을 찾음
 		{
-			if(isDead(i)) continue;
+			if(isDead(i)) continue; //이미 죽은 사람은 고려x
 			if(g_val < g[i])
 			{
 				g_idx = i;
-			    g_val = g[i];
+			   	g_val = g[i];
 			}
 		}
-		state |= (1<<g_idx);
-		ret = max(ret, Solve(cnt-1));
-		state &= ~(1<<g_idx);
+		state |= (1<<g_idx); //그 사람을 죽임
+		ret = max(ret, Solve(cnt-1)); //다음 경우를 고려
+		state &= ~(1<<g_idx); //백트래킹
 	}
 
-	else 
+	else  //홀수 : 밤의 경우
 	{
 		for(int i=0; i<n; i++)
 		{
-			if(i==mafia || isDead(i)) continue;
+			if(i==mafia || isDead(i)) continue;  //마피아 본인과, 이미 죽은사람 고려x
 
-			state |= (1<<i);
-			for(int j=0; j<n; j++) g[j] += r[i][j];
+			state |= (1<<i); //i번째 사람을 죽임
+			for(int j=0; j<n; j++) g[j] += r[i][j]; //+) 유죄 지수 갱신
 
-			ret = max(ret, Solve(cnt-1)+1);
+			ret = max(ret, Solve(cnt-1)+1); //다음 경우를 고려
 
-			state &= ~(1<<i);
 			for(int j=0; j<n; j++) g[j] -= r[i][j];
+			state &= ~(1<<i);  //백트래킹
 		}
 	}
 
@@ -70,7 +70,7 @@ int main()
 	cin>>n;
 
 	g = vector<int>(n, 0);
-	r = vector<vector<int> >(n+1, vector<int>(n, 0));
+	r = vector<vector<int> >(n, vector<int>(n, 0));
 
 	for(int i=0; i<n; i++)
 		cin>>g[i];
