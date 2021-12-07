29a9e6be3f49449af835c0a742c379d45c8263fb
Jaeho Lee <uss425@naver.com>
2021-12-07 15:23:57 +0900

    Update 1939_중량제한.cpp

--- Sources/1939_중량제한.cpp
+++ Sources/1939_중량제한.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 typedef pair<int, int> P;
 
-vector<P> adj[10001];
+vector<P> adj[10001]; //인접 리스트!
 int n, m, lo=1, hi=1;
 int st, fin, ans;
 
-bool BFS(const int limit)
+bool BFS(const int weight) //한 번에 weight만큼 옮길 경우, 도착 지점까지의 이동이 가능한가?
 {
-	queue<int> q;
-	vector<bool> visited(n+1, false);
+	queue<int> q; //BFS 위한 que
+	vector<bool> visited(n+1, false); //중복 탐색 방지
 
 	q.push(st);
 	visited[st]=true;
@@ -19,18 +19,18 @@ bool BFS(const int limit)
 		int curr = q.front();
 		q.pop();
 
-		if(curr==fin) return true;
+		if(curr==fin) return true; //도착 지점까지 안전하게 갈 수 있다면 true
 
-		for(const auto &next : adj[curr])
+		for(const auto &next : adj[curr]) //curr에서 나가는 간선 탐색
 		{
-			if(visited[next.first]==true) continue;
-			if(next.second < limit) continue;
+			if(visited[next.first]==true) continue; //이미 방문한 정점은 pass
+			if(next.second < weight) continue; //중량제한으로 인해 가지 못하면 pass
 
-			q.push(next.first);
+			q.push(next.first); 
 			visited[next.first] = true;
 		}
 	}
-	return false;
+	return false; //도착 지점까지 갈 수 없으니 false 반환
 }
 
 int main()
@@ -38,7 +38,7 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin>>n>>m;
+	cin>>n>>m; 
 	for(int i=0; i<m; i++)
 	{
 		int u, v, w;
@@ -53,12 +53,12 @@ int main()
 	{
 		int mid = (lo+hi)/2;
 
-		if(BFS(mid)) //가능하다면
+		if(BFS(mid)) //mid 무게는 이동이 가능하다
 		{
-			ans = mid; //정답을 갱신하고
-			lo = mid + 1; //더 높은 구간을 탐색
+			ans = mid; //그러면 정답을 갱신하고
+			lo = mid + 1; //mid 보다 더 큰 구간을 탐색
 		}
-		else hi = mid - 1; //불가능하면, 더 낮은 구간을 탐색
+		else hi = mid - 1; //불가능하면, mid모다 더 낮은 구간을 탐색
 	}
 
 	cout<<ans<<'\n';
