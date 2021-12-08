6ee157e7347cf132d08ba926e4ffcc4be4ab12fa
Jaeho Lee <uss425@naver.com>
2021-12-08 16:03:32 +0900

    Update 1300_K번째수.cpp

--- Sources/1300_K번째수.cpp
+++ Sources/1300_K번째수.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int n, k;
-int lo, hi, mid;
 
 int main()
 {
@@ -11,18 +10,17 @@ int main()
 
 	cin>>n>>k;
 
-	lo = 1;	hi = k;
-
-	while(lo <= hi)
+	int lo = 1, hi = k; //정답은 k보다 클 수 없음
+	while(lo <= hi) //이분 탐색
 	{
-		mid = (lo+hi)/2;
+		const int mid = (lo+hi)/2;
+	
+		int tmp=0; //2차원 배열에서 mid보다 작거나 같은 수의 개수
+		for(int i=1; i<=n; i++) //i번째 행에서의
+			tmp += min(mid/i, n); //작거나 같은 수들을 모두 세어줌
 
-		int tmp = 0;
-		for(int i=1; i<=n; i++)
-			tmp += min(mid/i, n);
-
-		if(tmp < k) lo = mid + 1;
-		else hi = mid - 1;
+		if(tmp >= k) hi = mid - 1; //범위를 좁힘
+		else lo = mid + 1; //범위를 좁힘
 	}
 
 	cout<<lo<<'\n';
