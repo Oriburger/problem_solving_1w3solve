3f9c66bf18b0ee4058076ea5095154fadcfe9ca9
Jaeho Lee <uss425@naver.com>
2021-12-16 18:02:56 +0900

    Update 2473_세_용액.cpp

--- BOJ/2473_세_용액.cpp
+++ BOJ/2473_세_용액.cpp
@@ -15,28 +15,27 @@ int main()
 	for(int i=0; i<n; i++)
 		cin>>arr[i];
 
-	sort(arr.begin(), arr.end());
+	sort(arr.begin(), arr.end()); //입력을 오름차순 정렬
 
-	for(int lo = 0; lo < arr.size()-2; lo++)
+	for(int lo = 0; lo < arr.size()-2; lo++) //lo를 기준으로 잡아둠
 	{
-		int mid = lo+1;
-		int hi = arr.size()-1;
+		int mid = lo+1; 
+		int hi = arr.size()-1; // [lo+1 ~ arr.size()-1] 구간에서 정답 탐색
 
-		while(mid < hi)
+		while(mid < hi) //투 포인터 탐색
 		{
 			long long temp = arr[lo]+arr[mid]+arr[hi];
 
-			if(abs(temp) < min_diff)
+			if(abs(temp) < min_diff) //정답을 찾았다면? 
 			{
 				min_diff = abs(temp);
 				ans = {arr[lo], arr[mid], arr[hi]};
 			}
-			if(temp < 0) mid++;
-			else if(temp > 0) hi--;
-			else break;
+			if(temp < 0) mid++; //음수라면 mid를 증가해서 0에 더 가까운 답을 찾는걸 꾀함
+			else if(temp > 0) hi--; //양수라면 반대
+			else break; //0이면 이미 가능한 가장 최적의 해
 		}
 	}
-	
 	cout<<ans[0]<<' '<<ans[1]<<' '<<ans[2]<<'\n';
 
 	return 0;
