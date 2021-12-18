2182f016b9388c5c7d85307fce7ce176e41fdc3b
Jaeho Lee <uss425@naver.com>
2021-12-18 15:32:54 +0900

    Update 1208_부분수열의합2.cpp

--- BOJ/1208_부분수열의합2.cpp
+++ BOJ/1208_부분수열의합2.cpp
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef unsigned long long ull;
+typedef long long ll;
 
-ull n, s, threshold, ans, sum;
-vector<ull> arr;
-map<ull, ull> cnt;
+ll n, s, tmp, ans;
+vector<ll> arr;
+unordered_map<int, int> umap;
 
 void DFS(int curr, bool bIsThreshold)
 {
-	if(curr == (bIsThreshold ? n : threshold))
+	if(curr == (!bIsThreshold ? n/2 : n))
 	{
 		if(!bIsThreshold) //시작점 ~ 중간지점
 		{
-			auto iter = cnt.find(sum); //합이 sum이 되는 수를 찾음
-			if(iter != cnt.end()) (iter->second) += 1; //있다면 개수를 더함
-			else cnt[sum] = 1; //없다면 map에 추가
+			if(umap.count(tmp)==0) umap[tmp] = 1; //합이 tmp가 되는 수를 찾음
+			else umap[tmp] += 1; //있다면 개수를 더함
 		}
 		else //도착점~중간지점
 		{
-			auto iter = cnt.find(s-sum); //합이 s-sum이 되는 수를 찾음
-			if(iter != cnt.end()) ans += iter->second; //있다면 정답 갱신
+			if(umap.count(s - tmp)!=0)  //합이 s-tmp가 되는 수를 찾음
+				ans += umap[s-tmp]; //있다면 정답 갱신
 		}
-		return;
+		return; 
 	}
 	DFS(curr+1, bIsThreshold); //curr을 pick하지 않음
-	sum += arr[curr];
+	tmp += arr[curr];
 	DFS(curr+1, bIsThreshold); //curr을 pick
-	sum -= arr[curr]; //backtrack
+	tmp -= arr[curr]; 
 }
 
 int main()
@@ -37,15 +36,14 @@ int main()
 
 	cin>>n>>s;
 
-	arr = vector<ull>(n);
+	arr = vector<ll>(n);
 	for(int i=0; i<n; i++)
 		cin>>arr[i];
-	threshold = n/2; //절반으로 나눔
+	
+	DFS(0, false);  //[0 ~ threshold-1] 탐색
+	DFS(n/2, true);  //[threshold ~ n-1] 탐색
 
-	DFS(0, false); //[0 ~ threshold-1] 탐색
-	DFS(threshold, true); //[threshold ~ n-1] 탐색
-
-	if(s==0) ans-=1; //아예 고르지 않는 상황은 제외 
+	if(s==0) ans-=1 ; //아예 고르지 않는 상황은 제외 
 	cout<<ans<<'\n';
 
 	return 0;
