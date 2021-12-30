8a9e7644275da9ab3a20f8bc75c35513edcd41b6
Jaeho Lee <uss425@naver.com>
2021-12-30 19:42:22 +0900

    Update 17471_게리맨더링.cpp

--- BOJ/17471_게리맨더링.cpp
+++ BOJ/17471_게리맨더링.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 const int INF = 2147000000;
 
-int n, m, ans = INF;
+int n, ans = INF;
 vector<int> p, adj[11];
 vector<int> combi, group[2], sum;
 
-int BFS(int start, const bool type)
+int BFS(int start, const bool type) //group[type]의 원소로 BFS
 {
 	int ret = p[start];
 	queue<int> q;
@@ -22,7 +22,7 @@ int BFS(int start, const bool type)
 		for(auto &next : adj[curr])
 		{
 			if(visited[next]) continue;
-			if(combi[next-1] != type) continue;
+			if(combi[next-1] != type) continue; //다른 그룹이라면 큐에 넣지 않음
 
 			q.push(next);
 			ret += p[next];
@@ -30,8 +30,8 @@ int BFS(int start, const bool type)
 		}
 	}
 	
-	for(auto &p : group[type])
-		if(!visited[p]) ret = -1;
+	for(auto &p : group[type]) //아직 방문하지 않은 그룹 내 정점이 있다면?
+		if(!visited[p]) ret = -1; //연결되어있지 않다는 의미이므로 -1반환
 
 	return ret;
 }
@@ -59,31 +59,25 @@ int main()
 		}		
 	}
 
-	int cnt = 0;
-	for(int len=1; len<=n/2; len++)
+	for(int len=1; len<=n/2; len++) // n/2번 반복
 	{	
-		combi.assign(len, 0);
-		for(int j=0; j<n-len; j++)
-			combi.push_back(1);
+		combi.assign(len, 0); //조합 만들기 (1) 
+		for(int j=0; j<n-len; j++) combi.push_back(1); //조합 만들기 (2)
 		do
 		{
-			group[0].clear(); group[1].clear();
-			sum = vector<int>(2, 0);
+			group[0].clear(); group[1].clear(); //컨테이너 
+			sum = vector<int>(2, 0);            //초기화
 
-			for(int i=0; i<n; i++)
+			for(int i=0; i<n; i++) //그룹에 맞게 컨테이너에 넣어줌
 				group[combi[i]].push_back(i+1);
-			for(int i=0; i<2; i++)
+			for(int i=0; i<2; i++) //각 그룹에서 BFS 
 				sum[i] = BFS(group[i][0], i);
 
-			if(sum[0]==-1 || sum[1]==-1) continue;
-
-			ans = min(ans, abs(sum[0]-sum[1]));
-
-		}while(next_permutation(combi.begin(), combi.end()));
-
+			if(sum[0]==-1 || sum[1]==-1) continue; //만약 그룹의 요소 중 하나가 떨어져있다면 continue
+			ans = min(ans, abs(sum[0]-sum[1])); //정답 갱신
+		}while(next_permutation(combi.begin(), combi.end())); //조합 만들기 (3)
 	}
-	
-	cout<<(ans == INF ? -1 : ans)<<'\n';
+	cout<<(ans == INF ? -1 : ans)<<'\n'; //정답 출력
 
 	return 0;
 }
