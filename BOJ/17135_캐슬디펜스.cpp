a8d3a29969a6f5db364f7fba55a46f41f27e0b03
Jaeho Lee <uss425@naver.com>
2022-01-06 16:43:44 +0900

    Update 17135_캐슬디펜스.cpp

--- BOJ/17135_캐슬디펜스.cpp
+++ BOJ/17135_캐슬디펜스.cpp
@@ -8,45 +8,41 @@ vector<vector<int> > board;
 
 typedef pair<int, int> P;
 
-inline int GetDist(P p, P q)
-{
-	return abs(p.first-q.first) + abs(p.second - q.second);
-}
+inline int GetDist(P p, P q){ return abs(p.first-q.first) + abs(p.second - q.second); } //맨해튼 거리 계산법
 
 int GetAnswer(vector<vector<int> > board)
 {
 	int ret = 0;
-	vector<int> pos;
-	for(int i=0; i<m; i++)
-		if(archer[i]) pos.push_back(i);
+	vector<int> pos; //궁수들이 위치한 열을 저장할 배열
+	for(int i=0; i<m; i++)	if(archer[i]) pos.push_back(i); 
 
-	for(int r=n; r>=1; r--)
+	for(int r=n; r>=1; r--) //궁수들이 한 칸씩 올라가며 턴을 진행
 	{
-		vector<P> target(3, {-1, -1});
+		vector<P> target(3, {-1, -1}); //3명의 궁수들의 타겟이 될 적
 		for(int k=0; k<3; k++)
 		{
 			int min_dist = INF;
-			for(int j=0; j<m; j++)
-			for(int i=0; i<r; i++)
+			for(int j=0; j<m; j++) //왼쪽에서 -> 오른쪽으로 순회
+			for(int i=0; i<r; i++) //위에서 -> 아래로 순회
 			{
-				if(board[i][j]==0) continue;
-				int temp = GetDist(P(r, pos[k]), P(i, j));
+				if(board[i][j]==0) continue; //적이 없다면 continue
+				int temp = GetDist(P(r, pos[k]), P(i, j)); //적과의 거리 도출
 				
-				if(temp <= d && min_dist > temp)
+				if(temp <= d && min_dist > temp) //d 이하이고, 이전에 구했던 거리보다 더 짧다면
 				{
-					min_dist = temp;
-					target[k] = {i, j};
+					min_dist = temp; //타겟 갱신
+					target[k] = {i, j}; //타겟 갱신
 				}
 			}
 		}
 
-		for(auto &p : target)
+		for(auto &p : target) //3명의 궁수들은 타겟을 동시에 공격
 		{
-			if(p == P(-1, -1)) continue;
+			if(p == P(-1, -1)) continue; //타겟이 없다면 continue
 			if(board[p.first][p.second]==1)
 			{
-				ret+=1;
-				board[p.first][p.second] = 0;
+				ret+=1; //적을 카운팅
+				board[p.first][p.second] = 0; //적은 죽음
 			}
 		}
 	}
@@ -71,8 +67,8 @@ int main()
 
 	do
 	{		
-		ans = max(ans, GetAnswer(board));
-	}while(next_permutation(archer.begin(), archer.end()));
+		ans = max(ans, GetAnswer(board)); //최댓값을 찾아냄
+	}while(next_permutation(archer.begin(), archer.end())); //가능한 모든 조합을 시도
 
 	cout<<ans<<'\n';
 
