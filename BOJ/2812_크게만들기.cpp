9adb016302e73b29a953875233ebd7a7c424f044
Jaeho Lee <uss425@naver.com>
2022-02-21 12:12:10 +0900

    Update 2812_크게만들기.cpp

--- BOJ/2812_크게만들기.cpp
+++ BOJ/2812_크게만들기.cpp
@@ -13,28 +13,28 @@ int main()
 
     cin>>n>>k>>str;
 
-    k = n-k;
-    for(int i=0; i<n; i++)
+	k = n-k; //k개를 빼는 거에서 n-k개를 뽑는 걸로 문제를 재구성
+    for(int i=0; i<n; i++) //입력된 수를 한 자리씩 순회
     {
-        char curr = str[i];
-    	  char top = tmp.empty() ? '0' : tmp[tmp.size()-1];
-    	   	
-        if(tmp.size() + n-i > k)
+    	char curr = str[i];
+    	char top = tmp.empty() ? '0' : tmp[tmp.size()-1];
+    	
+        if(tmp.size() + n-i > k) //남아있는 수의 개수가 넉넉하다면?
         {
-            if(curr == top) tmp.push_back(curr);
-        	  else
-        	  {
-        	    	while(!tmp.empty())
-        		    {
-        			      if(tmp.size() + n-i == k) break;
-        			      if(curr <= top) break;
-        			      tmp.pop_back();
-        			      top = tmp.empty() ? '0' : tmp[tmp.size()-1];
-				        }
-				        tmp.push_back(curr);
-			      }
-        }
-		else 
+        	if(curr == top) tmp.push_back(curr); //top랑 curr이 같다면 그냥 push
+        	else
+        	{
+        		while(!tmp.empty()) //스택에서 pop(a. 스택이 빌 때 까지) 
+        		{
+        			if(tmp.size() + n-i == k) break; // b. 또는 남은 수가 넉넉하지 않다면
+        			if(curr <= top) break; // c. curr이 top보다 작거나 같다면 break;
+        			tmp.pop_back(); //스택 pop
+        			top = tmp.empty() ? '0' : tmp[tmp.size()-1]; //top 갱신
+				}
+				tmp.push_back(curr); //수를 넣어줌
+			}
+		}
+		else //남은 수들로 n-k를 채워야하는 경우?
 			tmp.push_back(curr);
     }
     cout<<tmp.substr(0, k)<<'\n';
