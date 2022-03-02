a6b5f50746ff427d64fa40e8aa753b7b5ccfdefe
Jaeho Lee <uss425@naver.com>
2022-03-02 14:28:29 +0900

    Update H-index.cpp

--- Programmers/H-index.cpp
+++ Programmers/H-index.cpp
@@ -5,25 +5,14 @@ using namespace std;
 
 typedef vector<int>::iterator viter;
 
-int solution(vector<int> arr)
+int solution(vector<int> citations) 
 {
     int answer = 0;
-    
-    sort(arr.begin(), arr.end()); //lower_bound 사용을 위해 정렬
-    
-    for(int h=arr.size(); h>=0; h--) //h는 최대 n이다.
+    sort(citations.begin(), citations.end()); //lower_bound 사용을 위해 정렬
+    for(int h=0; h<=citations.size(); h++)
     {
-        //h값 이상의 위치를 찾는다.
-        viter pos = lower_bound(arr.begin(), arr.end(), h);
-        
-        //찾을수가 없다면?
-        if(pos == arr.end()) continue;
-        //그렇지 않고, pos의 위치가 조건에 부합한다면
-        else if(arr.end()-pos >= h) 
-        {
-            answer = h; //정답은 갱신
-            break; //가장 큰 수부터 탐색을 진행하기 때문에, 최초로 찾은 값이 최대
-        }
+        int q = citations.end() - lower_bound(citations.begin(), citations.end(), h);
+        if(q >= h) answer = h; //h번이상 인용된 논문이 h편 이상이라면?
     }
     
     return answer;
