b2fe2d2b24589b27cb3c57059cbe847d593c2e16
Jaeho Lee <uss425@naver.com>
2022-03-05 16:25:58 +0900

    Update LifeBoat.cpp

--- Programmers/LifeBoat.cpp
+++ Programmers/LifeBoat.cpp
@@ -1,35 +1,31 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<int> people, int limit)
+typedef vector<int>::iterator iter;
+
+int solution(vector<int> people, int limit) 
 {
     int answer = 0;
+    vector<int> tmp;
+    
     sort(people.begin(), people.end());
-    
-    vector<int>::iterator lo, hi;
-    lo = people.begin();
-    hi = people.end()-1;
-    
-    while(1)
+    iter lo = people.begin(), hi = people.end()-1;
+    while(lo<=hi)
     {
-        if(lo >= hi)
+        if(*lo + *hi > limit)
         {
-            answer += (lo==hi); //lo(hi) 하나가 남았다면? 1추가
-            break;
+            hi--;
+            answer++;
         }
-        
-        if(*lo + *hi > limit) //만약 *lo와 *hi의 합이 limit를 초과한다면?
+        else
         {
-            answer++; //hi만 배를 태워 보낸다.
-            hi--; //hi는 1감소
-        }
-        else //둘을 보트에 태워보낼수있다면?
-        {
-            answer++; //둘다 보트에 태워보냄
-            lo++; hi--; //각각 1
+            lo++; hi--;
+            answer++;
         }
     }
-    return answer; //정답 반환
+    
+    return answer;
 }
