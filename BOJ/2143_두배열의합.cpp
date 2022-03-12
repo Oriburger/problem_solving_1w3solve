79841eb9a02f251580033f4fd528781d8b6883b5
Jaeho Lee <uss425@naver.com>
2022-03-12 22:16:43 +0900

    Update 2143_두배열의합.cpp

--- BOJ/2143_두배열의합.cpp
+++ BOJ/2143_두배열의합.cpp
@@ -20,7 +20,7 @@ int main()
 	cin>>t>>n;
 	
 	s_a = a = vector<ll>(n+1);
-	for(int i=1; i<=n; i++)
+	for(int i=1; i<=n; i++) //a배열에 대한 누적합 구하기
 	{
 		cin>>a[i];
 		s_a[i] = s_a[i-1] + a[i];
@@ -28,13 +28,13 @@ int main()
 	
 	cin>>m;
 	s_b = b = vector<ll>(m+1);
-	for(int i=1; i<=m; i++)
+	for(int i=1; i<=m; i++) //b배열에 대한 누적합 구하기
 	{
 		cin>>b[i];
 		s_b[i] = s_b[i-1] + b[i];
 	}
 	
-	for(int k=0; k<2; k++)
+	for(int k=0; k<2; k++) //모든 부분합 만들기
 	{
 		for(int i=0; i<(k==0 ? n : m); i++)
 		{
@@ -42,7 +42,7 @@ int main()
 			{
 				ll sum = (k==0 ? s_a[j] - s_a[i] : s_b[j] - s_b[i]);
 				if(mp[k].count(sum)==0) mp[k][sum]=1;
-				else mp[k][sum]+=1;
+				else mp[k][sum]+=1; 
 			}
 		}
 	}
@@ -53,11 +53,11 @@ int main()
 		ll val = it->first; 
 		ll cnt = it->second;
 		
-		iter p = mp[1].lower_bound(t-val);
+		iter p = mp[1].lower_bound(t-val); //t-val을 B의 부분합 중에서 찾음
 		
 		if(p==mp[1].end() || p->first + val != t) continue;
-		ans += (cnt * p->second);		
-	}
+		ans += (cnt * p->second);  //mp[0]에서 val이 나오는 부분합 개수 
+	}                                // * mp[1]에서 t-val이 나오는 부분합 개수를 더함 
 	
 	cout<<ans<<'\n';
 	
