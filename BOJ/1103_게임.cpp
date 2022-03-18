b5fe7c1eca2f11604efb7044e2923a5e043621e4
Jaeho Lee <uss425@naver.com>
2022-03-18 23:23:54 +0900

    Update 1103_게임.cpp

--- BOJ/1103_게임.cpp
+++ BOJ/1103_게임.cpp
@@ -1,41 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 
-const int INF = 2147000000;
 const int dy[4] = {-1, 1, 0, 0};
 const int dx[4] = {0, 0, -1, 1};
 
 bool flag = false;
-int n, m, board[50][50], ans;
+int n, m, board[50][50];
 int cache[50][50];
-bool visited[50][50];
+bool finished[50][50];
 
 int DFS(int y, int x)
-{
-	if(y<0 || x<0 || y>=n || x>=m || board[y][x]==-1) return 0;
-	if(visited[y][x])
-	{
-		cout<<-1<<'\n';
-		exit(0);
-	}
-	
-	int &ret = cache[y][x];
-	if(ret != -1) return ret;
+{   
+    int &ret = cache[y][x];    
+    if(ret != 0) 
+    {
+        flag = !finished[y][x];
+        return ret = (flag ? -1 : ret);
+    }
 	
 	ret = 1;
-	visited[y][x] = true;
 	for(int d=0; d<4; d++)
 	{
 		int ny = y + dy[d] * board[y][x];
 		int nx = x + dx[d] * board[y][x];
-		
+        if(ny<0 || nx<0 || ny>=n || nx>=m || board[ny][nx]==-1) continue;
+        
 		ret = max(ret, DFS(ny, nx)+1);
+        if(flag) return ret = -1;
 	}
-	visited[y][x] = false;
-	return ret;
+	finished[y][x]=true; 
+	return ret = (flag ? -1 : ret);
 }
 
 int main()
@@ -52,7 +48,6 @@ int main()
 			board[i][j] = (c=='H' ? -1 : c-'0');
 		}
 	
-	memset(cache, -1, sizeof(cache));
 	cout<<DFS(0, 0)<<'\n';
 	
 	return 0;
