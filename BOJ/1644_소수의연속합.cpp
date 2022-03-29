d40f88eb87627ef16ab9f32df83f7d61747547cf
Jaeho Lee <uss425@naver.com>
2022-03-29 17:48:20 +0900

    Update 1644_소수의연속합.cpp

--- BOJ/1644_소수의연속합.cpp
+++ BOJ/1644_소수의연속합.cpp
@@ -15,27 +15,25 @@ int main()
 	
 	cin>>n;
 	
-	isPrime[1]=false;
-	for(int i=2; i<=n; i++)
-		for(int j=2; i*j<=n; j++)
-			isPrime[i*j]=false;		
+	isPrime[1]=false; //1은 소수가 아님
+	for(int i=2; i<=n; i++) //에라토스테네스의-체
+		for(int j=2; i*j<=n; j++) //i*j가 n이 될 때 까지만 반복
+			isPrime[i*j]=false; 
 	
-	for(int i=2; i<=n; i++)
+	for(int i=2; i<=n; i++) //2~n 
 	{
-		if(!isPrime[i]) continue;
-		bool flag = false;
-		
-		q.push(i);
-		sum += i;
-		
-		while(sum > n)
-		{
-			sum -= q.front();
-			q.pop();
-		}
-		if(sum == n) ans++;
-	}
+		if(!isPrime[i]) continue; //소수가 아니면 continue
 	
+		q.push(i); //큐에 넣음
+		sum += i; //합에도 더함
+		
+		while(sum > n) //sum이 n보다 큰 경우
+		{
+			sum -= q.front(); //큐의 앞에서 꺼내서 합에서 빼줌
+			q.pop(); //큐에서 꺼냄
+		}
+		if(sum == n) ans++; //만약 합이 n이 되는 경우를 찾았다면 정답 +1
+	}
 	cout<<ans<<'\n';
 	
 	return 0;
