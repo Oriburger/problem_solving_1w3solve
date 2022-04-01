21676916d875e69aa3c128689220f37df4b12f9c
Jaeho Lee <uss425@naver.com>
2022-04-01 21:20:00 +0900

    Update 3107_IPv6.cpp

--- BOJ/3107_IPv6.cpp
+++ BOJ/3107_IPv6.cpp
@@ -16,13 +16,13 @@ int main()
 	int target = str.size();
 	for(int i=0; i<str.size()-1; i++)
 		if(str[i]==':' && str[i]==str[i+1])
-			target = i;
+			target = i; //::을 찾는다.
 	
 	int lo = 0, hi = 7, pos = 3;
 	string tmp = "";
-	for(int i=0; i<=target; i++)
+	for(int i=0; i<=target; i++) //앞에서부터 ::까지 채워나감
 	{
-		if(str[i]==':' || i==target)
+		if(str[i]==':' || i==target) //:을 찾거나 마지막에 다다르면
 		{
 			arr[lo]="";
 			for(int j=0; j<4-tmp.size(); j++)
@@ -35,7 +35,7 @@ int main()
 		
 		tmp.push_back(str[i]);	
 	}
-	for(int i=str.size()-1; i>target; i--)
+	for(int i=str.size()-1; i>target; i--) //뒤에서부터 ::까지 채움
 	{
 		if(str[i]==':')
 		{
@@ -46,7 +46,7 @@ int main()
 		arr[hi][pos--] = str[i];
 	}
 	
-	for(int i=0; i<arr.size(); i++)
+	for(int i=0; i<arr.size(); i++) //정답 출력
 	{
 		cout<<arr[i];
 		if(i==arr.size()-1) break;
