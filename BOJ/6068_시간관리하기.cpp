6b8985ec2889f05e5a499da60289109b7a9b76cb
Jaeho Lee <uss425@naver.com>
2022-06-25 21:53:27 +0900

    Create 6068_시간관리하기.cpp

---
+++ BOJ/6068_시간관리하기.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+typedef pair<int, int> P;
+
+int n, k;
+vector<P> arr;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int a, b; cin>>a>>b;
+        arr.push_back({a, b});
+        k = max(k, b);
+    }
+    sort(arr.begin(), arr.end(), [](P p1, P p2) -> bool  //lambda!
+    {
+        if(p1.second == p2.second) return p1.first < p2.first;
+        return p1.second > p2.second;
+    });
+
+    for(auto &p : arr)
+    {
+        k = min(k, p.second);
+        k -= p.first;
+    }
+    k = (k<0 ? -1 : k);
+    cout<<k<<'\n';
+
+    return 0;
+}
