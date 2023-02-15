fdf5bf1cc313ffcdf0437307628fb13335c7d0b9
Jaeho Lee <uss425@naver.com>
2023-02-15 20:25:56 +0900

    Create 17287_TheDeeperTheBetter.cpp

---
+++ BOJ/17287_TheDeeperTheBetter.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+char st[3] = {'(', '{', '['};
+char fin[3] = {')', '}', ']'};
+string str; int ans = 0;
+
+int getHeadIdx(const char& c)
+{
+	for(int i=0; i<3; i++)
+		if(c == st[i]) return i;
+	return -1;
+}
+int getTailIdx(const char& c)
+{
+	for(int i=0; i<3; i++)
+		if(c == fin[i]) return i;
+	return -1;
+}
+
+int getScore(int idx)
+{
+	stack<char> stk;
+	int score = 0;
+	
+	for(int i=0; i<idx; i++)
+	{
+		const char& c = str[i];
+		if(c-'0' >= 0 && c-'0' <= 9) continue;
+		
+		int headIdx = getHeadIdx(c);
+		if(headIdx != -1)
+		{
+			stk.push(c);
+		}
+		else 
+		{
+			const char& t = stk.top();
+			int tailIdx = headIdx;
+			if(headIdx == tailIdx)
+				stk.pop();
+		}
+	}
+
+	while(!stk.empty())
+	{
+		if(stk.top() == '(') score += 1;
+		else if(stk.top() == '{') score += 2;
+		else if(stk.top() == '[') score += 3;
+		stk.pop();
+	}
+	return score;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin>>str;
+	
+	for(int i=0; i<str.length(); i++)
+	{
+		const char& c = str[i];
+
+		if(c-'0' >= 0 && c-'0' <= 9)
+			ans = max(ans, getScore(i));
+	}
+	cout<<ans<<'\n';
+	
+	return 0;
+}
