691f7cf1fe84751dac9edf469c77be28d6800655
Jaeho Lee <uss425@naver.com>
2023-04-02 00:12:51 +0900

    Update and rename BOJ/Undone/11536_줄세우기.cpp to BOJ/11536_줄세우기.cpp

--- BOJ/11536_줄세우기.cpp
+++ BOJ/11536_줄세우기.cpp
@@ -15,15 +15,16 @@ int main()
 	for(int i=0; i<n; i++)
 		cin>>str[i];
 
-	bool bIsDecrease = ((str[1][0] - str[0][0]) > 0);
-	bool bIsIncrease = ((str[1][0] - str[0][0]) < 0);
+	bool bIsDecrease = str[1] <= str[0];
+	bool bIsIncrease = str[1] >= str[0];
 	for(int i=2; i<n; i++)
 	{
-		if(bIsDecrease && str[1][0] - str[0][0] < 0) bIsDecrease = false;
-		if(bIsIncrease && str[1][0] - str[0][0] < 0) bIsIncrease = false;
+		if(bIsDecrease && str[i][0] > str[i-1][0]) bIsDecrease = false;
+		if(bIsIncrease && str[i][0] < str[i-1][0]) bIsIncrease = false;
 	}
 	if(bIsIncrease) cout<<"INCREASING\n";
-	
+	else if(bIsDecrease) cout<<"DECREASING\n";
+	else cout<<"NEITHER\n";
 	
 	return 0;
 }
