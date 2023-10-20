9b191aa9f7994349a0649725647229a5e07d5ef3
Jaeho Lee <uss425@naver.com>
2023-10-20 16:17:00 +0900

    Update 1655_가운데를말해요.cpp

--- BOJ/1655_가운데를말해요.cpp
+++ BOJ/1655_가운데를말해요.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int n;
 
-priority_queue<int, vector<int>, less<int> > minHeap;
-priority_queue<int, vector<int>, greater<int> > maxHeap;
+priority_queue<int, vector<int>, less<int> > maxHeap;
+priority_queue<int, vector<int>, greater<int> > minHeap;
 
 int main()
 {
@@ -20,18 +20,18 @@ int main()
 	{
 		int k; cin>>k; 
 		
-		if(i%2==1) maxHeap.push(k);
-		else minHeap.push(k);
+		if(i%2==1) minHeap.push(k);
+		else maxHeap.push(k);
 		
-		if(i>1 && maxHeap.top() < minHeap.top())
+		if(i>1 && minHeap.top() < maxHeap.top())
 		{
-			int a = minHeap.top(), b = maxHeap.top();
-			minHeap.pop(); maxHeap.pop();
-			minHeap.push(b); maxHeap.push(a);
+			int a = maxHeap.top(), b = minHeap.top();
+			maxHeap.pop(); minHeap.pop();
+			maxHeap.push(b); minHeap.push(a);
 		}
 		
-		if(i%2==1) cout<<maxHeap.top()<<'\n';
-		else cout<<min(maxHeap.top(), minHeap.top())<<'\n';
+		if(i%2==1) cout<<minHeap.top()<<'\n';
+		else cout<<min(minHeap.top(), maxHeap.top())<<'\n';
 	}	
 	return 0;
 } 
