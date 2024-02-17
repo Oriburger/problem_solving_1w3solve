4e57389867ec111d4141617a220ccd344fba13b7
Jaeho Lee <uss425@naver.com>
2024-02-18 02:56:12 +0900

    Update 10971_외판원순회2.cpp

--- BOJ/10971_외판원순회2.cpp
+++ BOJ/10971_외판원순회2.cpp
@@ -3,27 +3,8 @@
 #include <algorithm>
 using namespace std;
 
-int n;
-int w[10][10];
-
-int TSP(int start, int curr, int visited, int total = 0)
-{
-	if(visited == (1<<n+1) - 1)
-		return w[curr][start] == 0 ? 1e9 : w[curr][start];
-	
-	int ret = 1e9;
-	for(int i=0; i<n; i++)
-	{
-		if(visited & (1<<i)) continue;
-		if(w[curr][i] == 0) continue;
-	
-		visited |= (1<<i); 
-		ret = min(ret, TSP(start, i, visited, total + w[curr][i]) + w[curr][i]);
-		visited &= ~(1<<i);
-	}
-	
-	return ret;
-}
+int n, w[10][10], ans = 1e8;
+vector<int> route; 
 
 int main()
 {
@@ -33,17 +14,23 @@ int main()
 	cin>>n;
 	
 	for(int i=0; i<n; i++)
+	{
 		for(int j=0; j<n; j++)
 			cin>>w[i][j];
-		
-	int ans = 1e9, visited = 1<<n;
-	for(int i=0; i<n; i++)
-	{
-		visited |= (1<<i);
-		ans = min(ans, TSP(i, i, visited));	
-		visited &= ~(1<<i);
+		route.push_back(i);
 	}
-	
+			
+	do
+	{
+		int sum = w[route[n-1]][route[0]];
+		sum = (sum == 0 ? 1e8 : sum);
+		for(int i=1; i<n; i++)
+		{
+			const int dist = w[route[i-1]][route[i]];
+			sum += (dist == 0 ? 1e8 : dist);
+		}
+		ans = min(ans, sum);
+	}while(next_permutation(route.begin(), route.end()));
 	cout<<ans<<'\n';
 	
 	return 0;
