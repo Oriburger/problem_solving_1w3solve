65e602df9e7d453d5a618ab04581513bf72ac6b0
Jaeho Lee <uss425@naver.com>
2024-03-09 13:03:52 +0900

    Create 24445_알고리즘수업_너비우선탐색2.cpp

---
+++ BOJ/24445_알고리즘수업_너비우선탐색2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX_N 100001
+using namespace std;
+
+int n, m, r, cnt = 0;
+vector<int> adj[MAX_N];
+vector<int> visited(MAX_N, false);
+queue<int> q;
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin>>n>>m>>r; 
+	
+	for(int i=0; i<m; i++)
+	{
+		int u, v;
+		cin>>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	for(int i=1; i<=n; i++)
+		sort(adj[i].begin(), adj[i].end(), greater<int>());
+		
+	q.push(r);
+	visited[r] = ++cnt;
+	
+	while(!q.empty())
+	{
+		int curr = q.front();
+		q.pop();
+		
+		for(auto &next : adj[curr])
+		{
+			if(visited[next]) continue;
+			visited[next] = ++cnt;
+			q.push(next);
+		}
+	}
+	
+	for(int i=1; i<=n; i++)
+		cout<<visited[i]<<'\n';
+	
+	return 0;
+}
